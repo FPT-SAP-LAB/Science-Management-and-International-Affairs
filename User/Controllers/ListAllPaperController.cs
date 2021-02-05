83646af2fdfe6ce11e097ec28b2fc819c86044b7
hvu0163 <53392914+hvu0163@users.noreply.github.com>
2021-02-05 23:57:01 +0700

    Update ListAllPaperController.cs

--- User/Controllers/ListAllPaperController.cs
+++ User/Controllers/ListAllPaperController.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using User.Models;
 
 namespace User.Controllers
 {
     public class ListAllPaperController : Controller
     {
         // GET: ListAllPaper
-        [Route("paper/list")]
+        //[Route("paper/list")]
         public ActionResult Index()
         {
             ViewBag.title = "Sản phẩm khoa học";
-            var pagesTree = new List<String> { "Trang chủ", "Sản phẩm khoa học" };
+            var pagesTree = new List<PageTree>
+            {
+                new PageTree("Sản phẩm khoa học","/ListAllPaper"),
+            };
             ViewBag.pagesTree = pagesTree;
             return View();
         }
