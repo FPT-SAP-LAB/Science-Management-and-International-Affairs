5e60c9c888d3d7dbfbfd16c7b2e3a54bddaa0c6b
hvu0163 <53392914+hvu0163@users.noreply.github.com>
2021-02-05 21:16:05 +0700

    [add] Màn user sản phẩm khoa học
    Add ListAllPaperController, ListAllPaper.js, Index.cshtml
    Fix Admin.csproj, User.csproj

---
+++ User/Controllers/ListAllPaperController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace User.Controllers
+{
+    public class ListAllPaperController : Controller
+    {
+        // GET: ListAllPaper
+        [Route("paper/list")]
+        public ActionResult Index()
+        {
+            ViewBag.title = "Sản phẩm khoa học";
+            var pagesTree = new List<String> { "Trang chủ", "Sản phẩm khoa học" };
+            ViewBag.pagesTree = pagesTree;
+            return View();
+        }
+    }
+}
