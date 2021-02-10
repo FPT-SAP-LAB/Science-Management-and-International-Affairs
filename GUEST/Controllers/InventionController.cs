2825e1ebfa0e9e71ffa892c38c56f5ffeb01c1a2
hvu0163 <53392914+hvu0163@users.noreply.github.com>
2021-02-10 19:39:19 +0700

    [add] màn bằng sáng chế user (#49)
    * [add] màn bằng sáng chế user

---
+++ GUEST/Controllers/InventionController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using User.Models;
+
+namespace GUEST.Controllers
+{
+    public class InventionController : Controller
+    {
+        // GET: Invention
+        public ActionResult ListAll()
+        {
+            ViewBag.title = "Bằng sáng chế";
+            var pagesTree = new List<PageTree>
+            {
+                new PageTree("Bằng sáng chế","/Invention/ListAll"),
+            };
+            ViewBag.pagesTree = pagesTree;
+            return View();
+        }
+    }
+}
