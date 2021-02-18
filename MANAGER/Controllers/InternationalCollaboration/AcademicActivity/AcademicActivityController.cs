a439c45bc97e8b28841bb57c0d5e9e4b4a2169c2
duongpl <shidoundie@gmail.com>
2021-02-19 00:54:54 +0700

    [add] - adding AcademicActivityController and front-end

---
+++ MANAGER/Controllers/InternationalCollaboration/AcademicActivity/AcademicActivityController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MANAGER.Controllers.InternationalCollaboration.AcademicActivity
+{
+    public class AcademicActivityController : Controller
+    {
+        // GET: AcademicActivity
+        public ActionResult List()
+        {
+            ViewBag.Title = "Danh sách hoạt động trong năm";
+            return View();
+        }
+    }
+}
