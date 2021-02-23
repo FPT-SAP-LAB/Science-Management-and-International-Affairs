842cf15fae23f421f49c3ee0a9e186078b5d05bd
duongpl <shidoundie@gmail.com>
2021-02-24 00:04:59 +0700

    [fix] - resolve conflic

---
+++ MANAGER/Controllers/InternationalCollaboration/AcademicActivity/CheckInController.cs
@@ -0,0 +1,44 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MANAGER.Controllers.InternationalCollaboration.AcademicActivity
+{
+    public class CheckInController : Controller
+    {
+        // GET: CheckIn
+        public ActionResult List()
+        {
+            ViewBag.pageTitle = "Check-in hoạt động học thuật trong năm";
+            return View();
+        }
+        public JsonResult add_CheckIn(string tu_cach, string name, string email, string don_vi, string co_so)
+        {
+            try
+            {
+                JsonError jerr = new JsonError()
+                {
+                    code = 1,
+                    err_content = "Đã thêm thành công"
+                };
+                return Json(jerr, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception)
+            {
+                JsonError jerr = new JsonError()
+                {
+                    code = 2,
+                    err_content = "Có lỗi xảy ra. Vui lòng thử lại"
+                };
+                return Json(jerr, JsonRequestBehavior.AllowGet);
+            }
+        }
+        private class JsonError
+        {
+            public int code;
+            public string err_content;
+        }
+    }
+}
