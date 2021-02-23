e3d218f13aa8584da8ff65e6ccd41b4c68f8deb8
duongpl <51127228+duongpl@users.noreply.github.com>
2021-02-24 00:06:53 +0700

    [fix] - resolve conflic (#82)
    * Revert "[add] - create Scholarship controller and list view (#64)"
    
    This reverts commit 59dfffbb577380591aa2ceecb1ced2ba9429db57.
    
    * Revert "Revert "[add] - create Scholarship controller and list view (#64)""
    
    This reverts commit 5f0dddc69eb1566e030cea020be6a89d7c23881a.
    
    * Revert "[add] - create Scholarship controller and list view (#64)"
    
    This reverts commit 59dfffbb577380591aa2ceecb1ced2ba9429db57.
    
    * [fix] - AcademicActivity front-end and InternationalCollaboration layout
    
    * [fix] - fix AcademicActivity front-end
    
    * [fix,add] - fix AcademicActivity front-end and add Add,Edit,Delete in controller
    
    * Delete pre-commit
    
    * Create pre-commit
    
    * [fix,add] - fix AcademicActivity front-end and add Add,Edit,Delete in AcademicActivityController
    
    * [add - fix] - add CheckInController + front-end and link concern pages to each other
    
    * Revert "[add - fix] - add CheckInController + front-end and link concern pages to each other (#81)"
    
    This reverts commit 20654c6c76039352b7223cb774c1c0182dea36b2.
    
    * [fix] - resolve conflic
    
    * Update CheckInController.cs

--- MANAGER/Controllers/InternationalCollaboration/AcademicActivity/AcademicActivityController.cs
+++ MANAGER/Controllers/InternationalCollaboration/AcademicActivity/AcademicActivityController.cs
@@ -14,5 +14,73 @@ namespace MANAGER.Controllers.InternationalCollaboration.AcademicActivity
             ViewBag.pageTitle = "Danh sách hoạt động học thuật trong năm";
             return View();
         }
+        public JsonResult delete_AcademicActivity(int id)
+        {
+            try
+            {
+                JsonError jerr = new JsonError()
+                {
+                    code = 1,
+                    err_content = "Đã xóa thành công"
+                };
+                return Json(jerr, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception)
+            {
+                JsonError jerr = new JsonError()
+                {
+                    code = 2,
+                    err_content = "Có lỗi xảy ra. Vui lòng thử lại"
+                };
+                return Json(jerr, JsonRequestBehavior.AllowGet);
+            }
+        }
+        public JsonResult add_AcademicActivity(string title, string category, string from, string to, string location)
+        {
+            try
+            {
+                JsonError jerr = new JsonError()
+                {
+                    code = 1,
+                    err_content = "Đã thêm thành công"
+                };
+                return Json(jerr, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception)
+            {
+                JsonError jerr = new JsonError()
+                {
+                    code = 2,
+                    err_content = "Có lỗi xảy ra. Vui lòng thử lại"
+                };
+                return Json(jerr, JsonRequestBehavior.AllowGet);
+            }
+        }
+        public JsonResult edit_AcademicActivity(string title, string category, string from, string to, string location)
+        {
+            try
+            {
+                JsonError jerr = new JsonError()
+                {
+                    code = 1,
+                    err_content = "Đã chỉnh sửa thành công"
+                };
+                return Json(jerr, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception)
+            {
+                JsonError jerr = new JsonError()
+                {
+                    code = 2,
+                    err_content = "Có lỗi xảy ra. Vui lòng thử lại"
+                };
+                return Json(jerr, JsonRequestBehavior.AllowGet);
+            }
+        }
+        private class JsonError
+        {
+            public int code;
+            public string err_content;
+        }
     }
 }
