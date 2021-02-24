1805f4902e1b6002c2009bc398328d51e960741b
duongpl <51127228+duongpl@users.noreply.github.com>
2021-02-24 23:41:19 +0700

    [add] - add DetailOfAcademicActitivyController + front-end and add script ckeditor (#85)
    * Revert "[add] - create Scholarship controller and list view (#64)"
    
    This reverts commit 59dfffbb577380591aa2ceecb1ced2ba9429db57.
    
    * Revert "Revert "[add] - create Scholarship controller and list view (#64)""
    
    This reverts commit 5f0dddc69eb1566e030cea020be6a89d7c23881a.
    
    * Revert "[add] - create Scholarship controller and list view (#64)"
    
    This reverts commit 59dfffbb577380591aa2ceecb1ced2ba9429db57.
    
    * [fix] - AcademicActivity front-end and InternationalCollaboration layout
    
    * [fix] - fix AcademicActivity front-end
    
    * [fix,add] - fix AcademicActivity front-end and add Add,Edit,Delete in controller
    
    * Delete pre-commit
    
    * Create pre-commit
    
    * [fix,add] - fix AcademicActivity front-end and add Add,Edit,Delete in AcademicActivityController
    
    * [add - fix] - add CheckInController + front-end and link concern pages to each other
    
    * Revert "[add - fix] - add CheckInController + front-end and link concern pages to each other (#81)"
    
    This reverts commit 20654c6c76039352b7223cb774c1c0182dea36b2.
    
    * [fix] - resolve conflic
    
    * Update CheckInController.cs
    
    * [add] - add DetailOfAcademicActitivyController + front-end and add script ckeditor
    
    * Update DetailOfAcademicActivityController.cs

---
+++ MANAGER/Models/JsonError.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MANAGER.Models
+{
+    public class JsonError
+    {
+        public int code { get; set; }
+        public string err_content { get; set; }
+    }
+}
