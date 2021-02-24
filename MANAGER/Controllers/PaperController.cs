435ee16fccfe932fff94c7beb3981eb590211214
hvu0163 <53392914+hvu0163@users.noreply.github.com>
2021-02-24 10:16:16 +0700

    [add] màn list paper manager

---
+++ MANAGER/Controllers/PaperController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MANAGER.Controllers
+{
+    public class PaperController : Controller
+    {
+        // GET: Paper
+        public ActionResult Pending()
+        {
+            ViewBag.title = "Danh sách bài báo đang chờ xét duyệt";
+            return View();
+        }
+
+        public ActionResult Detail()
+        {
+            ViewBag.title = "Chi tiết bài báo";
+            return View();
+        }
+    }
+}
