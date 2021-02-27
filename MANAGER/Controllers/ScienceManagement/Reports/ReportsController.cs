d15046537c068dcd5669860c41fa470f935a7f7f
Quoc Tuan <39939107+quoctuanck99@users.noreply.github.com>
2021-02-28 02:31:20 +0700

    [add] Report: International Papers and Paper by Workplace

---
+++ MANAGER/Controllers/ScienceManagement/Reports/ReportsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MANAGER.Controllers.ScienceManagement.Reports
+{
+    public class ReportsController : Controller
+    {
+        // GET: Reports
+        public ActionResult PapersReportsByWorkplace()
+        {
+            ViewBag.title = "THỐNG KÊ BÀI BÁO KHOA HỌC THEO KHU VỰC";
+            return View();
+        }
+        public ActionResult InternationalPapersReports()
+        {
+            ViewBag.title = "THỐNG KÊ KHEN THƯỞNG BÀI BÁO QUỐC TẾ ";
+            return View();
+        }
+        //public ActionResult InCountryPapersReports()
+        //{
+        //    ViewBag.title = "THỐNG KÊ KHEN THƯỞNG BÀI BÁO QUỐC TẾ ";
+        //    return View();
+        //}
+        //public ActionResult IntellectualPropertyReports()
+        //{
+        //    ViewBag.title = "THỐNG KÊ KHEN THƯỞNG BÀI BÁO QUỐC TẾ ";
+        //    return View();
+        //}
+
+    }
+}
