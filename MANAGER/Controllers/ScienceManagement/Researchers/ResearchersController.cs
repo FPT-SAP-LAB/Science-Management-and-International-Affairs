01427dcb3f121be07006a843198485b5669b472c
Quoc Tuan <39939107+quoctuanck99@users.noreply.github.com>
2021-03-02 00:56:08 +0700

    [add] List Researcher and Error Page (#125)
    * [fix] scholarship list (Son Nguyen)
    
    * [add] Login page
    
    * [add][fix] Fix layout - login - home page
    
    * [add] Report: International Papers and Paper by Workplace
    
    * [add] In Country Papers Report
    
    * [add] Intellectual Property Report
    
    * [add] Citations Report
    
    * [add] Conference Participation Report
    
    * [add] author reward  Report
    
    * [add] List of paid income
    
    * [add] Dashboard raw layout
    
    * [fix] css for dashboard page
    
    * [add] List Researcher and Error Page

---
+++ MANAGER/Controllers/ScienceManagement/Researchers/ResearchersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MANAGER.Controllers.ScienceManagement.Researchers
+{
+    public class ResearchersController : Controller
+    {
+        // GET: Researchers
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
