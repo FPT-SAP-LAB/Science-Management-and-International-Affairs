f7e03ef1953ea276e1e9c6c4ce83331c6dbe58b5
Đăng Dũng <47218583+Ctbom@users.noreply.github.com>
2021-03-02 21:26:39 +0700

    [add] backend MOU (#131)
    * [add] Update file in MOU Module in BLL and Manager
    
    * [fix] fix bug from CodeFactor
    
    * [add] backend MOU
    
    * [fix] fix bug from CodeFactor

--- BLL/InternationalCollaboration/Collaboration/MemorandumOfUnderstanding/MOURepo.cs
+++ BLL/InternationalCollaboration/Collaboration/MemorandumOfUnderstanding/MOURepo.cs
@@ -1,6 +1,8 @@
 ﻿using ENTITIES;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +15,182 @@ namespace BLL.InternationalCollaboration.Collaboration.MemorandumOfUnderstanding
 
         public List<ListMOU> listAllMOU()
         {
-            return new List<ListMOU>();
+            try
+            {
+                string sql_mouList = $"";
+                List<ListMOU> mouList = db.Database.SqlQuery<ListMOU>(sql_mouList).ToList();
+                handlingMOUListData(mouList);
+                return mouList;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public List<ListMOU> listAllMOUDeleted()
+        {
+            try
+            {
+                string sql_mouList_deleted = $"";
+                List<ListMOU> mouList = db.Database.SqlQuery<ListMOU>(sql_mouList_deleted).ToList();
+                handlingMOUListData(mouList);
+                return mouList;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void addMOU(MOUAdd input)
+        {
+            using (DbContextTransaction transaction = db.Database.BeginTransaction())
+            {
+                try
+                {
+                    db.MOUs.Add(input.MOU);
+                    db.MOUStatusHistories.Add(input.MOUStatusHistory);
+                    foreach (MOUPartner item in input.listMOUPartner)
+                    {
+                        db.MOUPartners.Add(item);
+                    }
+                    foreach (MOUPartnerSpecialization item in input.listMOUPartnerSpe)
+                    {
+                        db.MOUPartnerSpecializations.Add(item);
+                    }
+                    foreach (MOUPartnerScope item in input.listMOUPartnerScope)
+                    {
+                        db.MOUPartnerScopes.Add(item);
+                    }
+                    db.SaveChanges();
+                    transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    throw ex;
+                }
+            }
+        }
+
+        public void ExportMOUExcel()
+        {
+            //data of excel sheet??
+        }
+
+        public void deleteMOU(int mou_id)
+        {
+            using (DbContextTransaction transaction = db.Database.BeginTransaction())
+            {
+                try
+                {
+                    MOU mou = db.MOUs.Find(mou_id);
+                    mou.is_deleted = true;
+                    db.Entry(mou).State = EntityState.Modified;
+                    db.SaveChanges();
+                    transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback();
+                    throw ex;
+                }
+            }
+        }
+
+        public string getSuggestedMOUCode()
+        {
+            try
+            {
+                string sql_mouCode = $"";
+                string newMOUCode = db.Database.SqlQuery<string>(sql_mouCode).First();
+                return newMOUCode;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool partnerIsExisted(int partner_id)
+        {
+            try
+            {
+                string sql_partner = $"";
+                List<Partner> partner = db.Database.SqlQuery<Partner>(sql_partner).ToList();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public List<InternalUnit> GetInternalUnit()
+        {
+            try
+            {
+                string sql_unitList = $"";
+                List<InternalUnit> unitList = db.Database.SqlQuery<InternalUnit>(sql_unitList).ToList();
+                return unitList;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public List<Partner> GetPartners()
+        {
+            try
+            {
+                string sql_partnerList = $"";
+                List<Partner> partnerList = db.Database.SqlQuery<Partner>(sql_partnerList).ToList();
+                return partnerList;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public List<Specialization> GetSpecializations()
+        {
+            try
+            {
+                string sql_speList = $"";
+                List<Specialization> speList = db.Database.SqlQuery<Specialization>(sql_speList).ToList();
+                return speList;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public List<CollaborationScope> GetCollaborationScopes()
+        {
+            try
+            {
+                string sql_scopeList = $"";
+                List<CollaborationScope> scopeList = db.Database.SqlQuery<CollaborationScope>(sql_scopeList).ToList();
+                return scopeList;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void checkCollabStatus()
+        {
+            //?
+        }
+
+        private void handlingMOUListData(List<ListMOU> mouList)
+        {
+            //handling spe and scope data.
         }
 
         public int getDuration()
@@ -24,7 +201,40 @@ namespace BLL.InternationalCollaboration.Collaboration.MemorandumOfUnderstanding
             return value.Duration().Days;
         }
 
+        public NotificationInfo getNoti()
+        {
+            try
+            {
+                NotificationInfo noti = new NotificationInfo();
+                string sql_inactive_number = $"";
+                string sql_expired = $"";
+                noti.InactiveNumber = db.Database.SqlQuery<int>(sql_inactive_number).First();
+                noti.ExpiredMOUCode = db.Database.SqlQuery<string>(sql_expired).ToList();
+                return noti;
+            } catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void UpdateStatusMOU()
+        {
+            //get current date
+            //get all expired ActiveMOU.
+            //if number > 0: update status for MOU: Active => Inactive.
+        }
+
         public class ListMOU {}
+
+        public class MOUAdd 
+        {
+            public MOUAdd() { }
+            public MOU MOU { get; set; }
+            public MOUStatusHistory MOUStatusHistory { get; set; }
+            public List<MOUPartner> listMOUPartner { get; set; }
+            public List<MOUPartnerSpecialization> listMOUPartnerSpe { get; set; }
+            public List<MOUPartnerScope> listMOUPartnerScope { get; set; }
+        }
 
         public class NotificationInfo
         {
