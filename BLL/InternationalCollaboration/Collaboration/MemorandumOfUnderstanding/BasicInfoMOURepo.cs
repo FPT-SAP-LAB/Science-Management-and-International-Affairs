54e7cb5d18a4dc1c7b3730fa57d1b3f0658d0632
Ctbom <dungpdhe130254@fpt.edu.vn>
2021-03-04 08:16:46 +0700

    [add] add backend file

--- BLL/InternationalCollaboration/Collaboration/MemorandumOfUnderstanding/BasicInfoMOURepo.cs
+++ BLL/InternationalCollaboration/Collaboration/MemorandumOfUnderstanding/BasicInfoMOURepo.cs
@@ -10,9 +10,43 @@ namespace BLL.InternationalCollaboration.Collaboration.MemorandumOfUnderstanding
     class BasicInfoMOURepo
     {
         readonly ScienceAndInternationalAffairsEntities db = new ScienceAndInternationalAffairsEntities();
-        public void getBasicInfoMOU()
+        public MOU getBasicInfoMOU()
         {
-            return;
+            try
+            {
+                string sql_mouBasicInfo =
+                    @"select tb2.mou_partner_id,
+                        tb1.mou_code,tb3.partner_name,tb3.website,tb2.contact_point_name
+                        ,tb2.contact_point_email,tb2.contact_point_phone,tb1.evidence,
+                        tb2.mou_start_date, tb1.mou_end_date, tb1.mou_note, tb10.office_abbreviation, tb5.scope_abbreviation
+                        ,tb7.specialization_name, tb9.mou_status_name
+                        from IA_Collaboration.MOU tb1 inner join IA_Collaboration.MOUPartner tb2
+                        on tb1.mou_id = tb2.mou_id inner join IA_Collaboration.Partner tb3 
+                        on tb2.partner_id = tb3.partner_id inner join IA_Collaboration.MOUPartnerScope tb4
+                        on tb4.mou_id = tb2.mou_id and tb4.partner_id = tb2.partner_id
+                        inner join IA_MasterData.CollaborationScope tb5
+                        on tb4.scope_id = tb5.scope_id inner join IA_Collaboration.MOUPartnerSpecialization tb6
+                        on tb6.mou_partner_id = tb2.mou_partner_id 
+                        inner join General.Specialization tb7
+                        on tb7.specialization_id = tb6.specialization_id
+                        inner join 
+                        (select max([datetime]) as 'maxdate',mou_status_id, mou_id
+                        from IA_Collaboration.MOUStatusHistory 
+                        group by mou_status_id, mou_id) tb8 on
+                        tb8.mou_id = tb1.mou_id
+                        inner join IA_Collaboration.MOUStatus tb9 on
+                        tb9.mou_status_id = tb8.mou_status_id
+                        inner join General.Office tb10 on
+                        tb10.office_id = tb1.office_id
+                        where tb1.is_deleted = 0 ";
+                MOU mouBasicInfo = db.Database.SqlQuery<MOU>(sql_mouBasicInfo).First();
+                //handlingMOUListData(mouList);
+                return mouBasicInfo;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
         public void listAllExtraMOU()
         {
