08d98050cc83dc6e4fa732040b2aab7e17259c46
hvu0163 <53392914+hvu0163@users.noreply.github.com>
2021-03-04 14:50:41 +0700

    [fix] sửa thêm tác giả

---
+++ GUEST/Controllers/ScienceManagement/PartialViewController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GUEST.Controllers.ScienceManagement
+{
+    public class PartialViewController : Controller
+    {
+        // GET: PartialView
+        [ChildActionOnly]
+        public ActionResult AddAuthor()
+        {
+            return PartialView();
+        }
+    }
+}
