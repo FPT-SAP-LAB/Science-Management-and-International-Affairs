fb63224ece94325341ce8876dc997cc21a1f0dc8
Ctbom <dungpdhe130254@fpt.edu.vn>
2021-03-06 23:57:03 +0700

    [fix] fixing bug from MOU Module

--- BLL/InternationalCollaboration/Collaboration/MemorandumOfUnderstanding/MOURepo.cs
+++ BLL/InternationalCollaboration/Collaboration/MemorandumOfUnderstanding/MOURepo.cs
@@ -39,7 +39,7 @@ namespace BLL.InternationalCollaboration.Collaboration.MemorandumOfUnderstanding
                         from IA_Collaboration.MOUStatusHistory 
                         group by mou_status_id, mou_id) tb8 on
                         tb8.mou_id = tb1.mou_id
-                        inner join IA_Collaboration.MOUStatus tb9 on
+                        inner join IA_Collaboration.CollaborationStatus tb9 on
                         tb9.mou_status_id = tb8.mou_status_id
                         inner join General.Office tb10 on
                         tb10.office_id = tb1.office_id
@@ -66,28 +66,30 @@ namespace BLL.InternationalCollaboration.Collaboration.MemorandumOfUnderstanding
             try
             {
                 string sql_mouList_deleted = @"select tb2.mou_partner_id,
-                    tb1.mou_code,tb3.partner_name,tb3.website,tb2.contact_point_name
+                    tb1.mou_code,tb3.partner_name,tb11.country_name,tb3.website,tb2.contact_point_name
                     ,tb2.contact_point_email,tb2.contact_point_phone,tb1.evidence,
                     tb2.mou_start_date, tb1.mou_end_date, tb1.mou_note, tb10.office_abbreviation, tb5.scope_abbreviation
-                    ,tb7.specialization_name, tb9.mou_status_name
+                    ,tb7.specialization_name, tb9.mou_status_id,tb1.mou_id
                     from IA_Collaboration.MOU tb1 inner join IA_Collaboration.MOUPartner tb2
-                    on tb1.mou_id = tb2.mou_id inner join IA_Collaboration.Partner tb3
+                    on tb1.mou_id = tb2.mou_id inner join IA_Collaboration.Partner tb3 
                     on tb2.partner_id = tb3.partner_id inner join IA_Collaboration.MOUPartnerScope tb4
                     on tb4.mou_id = tb2.mou_id and tb4.partner_id = tb2.partner_id
                     inner join IA_MasterData.CollaborationScope tb5
                     on tb4.scope_id = tb5.scope_id inner join IA_Collaboration.MOUPartnerSpecialization tb6
-                    on tb6.mou_partner_id = tb2.mou_partner_id
+                    on tb6.mou_partner_id = tb2.mou_partner_id 
                     inner join General.Specialization tb7
                     on tb7.specialization_id = tb6.specialization_id
-                    inner join
+                    inner join 
                     (select max([datetime]) as 'maxdate',mou_status_id, mou_id
-                    from IA_Collaboration.MOUStatusHistory
+                    from IA_Collaboration.MOUStatusHistory 
                     group by mou_status_id, mou_id) tb8 on
                     tb8.mou_id = tb1.mou_id
-                    inner join IA_Collaboration.MOUStatus tb9 on
+                    inner join IA_Collaboration.CollaborationStatus tb9 on
                     tb9.mou_status_id = tb8.mou_status_id
                     inner join General.Office tb10 on
                     tb10.office_id = tb1.office_id
+                    inner join General.Country tb11 on 
+                    tb11.country_id = tb3.country_id
                     where tb1.is_deleted = 1";
                 List<ListMOU> mouList = db.Database.SqlQuery<ListMOU>(sql_mouList_deleted).ToList();
                 handlingMOUListData(mouList);
@@ -283,6 +285,11 @@ namespace BLL.InternationalCollaboration.Collaboration.MemorandumOfUnderstanding
                     t1.country_id = t2.country_id where t1.partner_name = @partner_name";
                 CustomPartner p = db.Database.SqlQuery<CustomPartner>(sql,
                     new SqlParameter("partner_name", partner_name)).FirstOrDefault();
+                if (p != null)
+                {
+                    p.website = p.website is null ? "" : p.website;
+                    p.address = p.address is null ? "" : p.address;
+                }
                 return p;
             }
             catch (Exception ex)
