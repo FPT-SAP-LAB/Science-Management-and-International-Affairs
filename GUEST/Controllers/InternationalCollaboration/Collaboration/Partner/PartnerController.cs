d1d07fa289506530923fa3df7f2caaafea5b265d
sonnh <yggdrasil2769@gmail.com>
2021-03-06 15:19:38 +0700

    [add] - add view partner guest project

---
+++ GUEST/Controllers/InternationalCollaboration/Collaboration/Partner/PartnerController.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using User.Models;
+
+namespace GUEST.Controllers.InternationalCollaboration.Collaboration.Partner
+{
+    public class PartnerController : Controller
+    {
+        // GET: Partner
+        public ActionResult List()
+        {
+            var pagesTree = new List<PageTree>
+            {
+                new PageTree("Danh sách đối tác", "Partner")
+            };
+            ViewBag.pagesTree = pagesTree;
+            return View();
+        }
+        public class ColorDeleteOutput
+        {
+            public List<ResponseMessage> Response { get; set; }
+        }
+
+        public class ResponseMessage
+        {
+            public string StyleNumber { get; set; }
+            public string ColorLongDesc { get; set; }
+            public string DistroDesc { get; set; }
+            public string Status { get; set; }
+        }
+
+        public ActionResult Load_List()
+        {
+            List<Array> respmsg = new List<Array>();
+
+
+            for (int i = 0; i < 100; i++)
+            {
+                string[] temp = { i + 1 + "", "SBI Graduate School", "Japan", "CNTT", "www"};
+                respmsg.Add(temp);
+                //respmsg.Add(new ResponseMessage { ColorLongDesc = i+"", StyleNumber = "124578", DistroDesc = "MARIO BRACKEN", Status = "SUCCESS" });
+            }
+            string sJSONResponse = JsonConvert.SerializeObject(respmsg);
+
+            return Json(new { success = true, data = respmsg }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
