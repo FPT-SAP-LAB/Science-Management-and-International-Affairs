1ffcf948524bd064ff93349e708d44e048065487
sonnh <yggdrasil2769@gmail.com>
2021-03-07 00:47:23 +0700

    [fix, add] - add detail partner view guest, fix datatable list partner guest

--- GUEST/Controllers/InternationalCollaboration/Collaboration/Partner/PartnerController.cs
+++ GUEST/Controllers/InternationalCollaboration/Collaboration/Partner/PartnerController.cs
@@ -20,33 +20,86 @@ namespace GUEST.Controllers.InternationalCollaboration.Collaboration.Partner
             ViewBag.pagesTree = pagesTree;
             return View();
         }
-        public class ColorDeleteOutput
-        {
-            public List<ResponseMessage> Response { get; set; }
-        }
 
-        public class ResponseMessage
+        //demo code back end thì xóa đi
+        //------------------------------------------------
+
+        class Person
         {
-            public string StyleNumber { get; set; }
-            public string ColorLongDesc { get; set; }
-            public string DistroDesc { get; set; }
-            public string Status { get; set; }
+            private string partner_name_mou_code; // field
+            private string country_name; // field
+            private string specialization; // field
+            private string website; // field
+
+            public Person()
+            {
+            }
+            public Person(string partner_name_mou_code, string country_name, string specialization, string website)
+            {
+                this.partner_name_mou_code = partner_name_mou_code;
+                this.country_name = country_name;
+                this.specialization = specialization;
+                this.website = website;
+            }
+
+            public string Partner_name_mou_code   // property
+            {
+                get { return partner_name_mou_code; }   // get method
+                set { partner_name_mou_code = value; }  // set method
+            }
+            public string Country_name   // property
+            {
+                get { return country_name; }   // get method
+                set { country_name = value; }  // set method
+            }
+            public string Specialization   // property
+            {
+                get { return specialization; }   // get method
+                set { specialization = value; }  // set method
+            }
+            public string Website   // property
+            {
+                get { return website; }   // get method
+                set { website = value; }  // set method
+            }
+
+
         }
+        //------------------------------------------------
 
         public ActionResult Load_List()
         {
-            List<Array> respmsg = new List<Array>();
+            List<Person> list = new List<Person>();
 
 
             for (int i = 0; i < 100; i++)
             {
-                string[] temp = { i + 1 + "", "SBI Graduate School", "Japan", "CNTT", "www"};
-                respmsg.Add(temp);
-                //respmsg.Add(new ResponseMessage { ColorLongDesc = i+"", StyleNumber = "124578", DistroDesc = "MARIO BRACKEN", Status = "SUCCESS" });
-            }
-            string sJSONResponse = JsonConvert.SerializeObject(respmsg);
+                Person temp = new Person();
+                if (i % 2 == 0)
+                {
+                    temp = new Person("2021/01:SBI Graduate School", "Japan", "CNNT", "www");
+                }
+                else
+                {
+                    temp = new Person("2021/01:SBI Graduate School", "Japan", "CNNT", "https://");
 
-            return Json(new { success = true, data = respmsg }, JsonRequestBehavior.AllowGet);
+                }
+                list.Add(temp);
+            }
+
+
+            return Json(new { success = true, data = list }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Partner_Detail(string id)
+        {
+            var pagesTree = new List<PageTree>
+            {
+                new PageTree("Danh sách đối tác", "/Partner/List"),
+                new PageTree("Thông tin chi tiết đối tác", "/Partner/Partner_Detail?" + id)
+            };
+            ViewBag.pagesTree = pagesTree;
+            return View();
         }
     }
 }
