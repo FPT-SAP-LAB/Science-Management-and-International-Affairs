22c1048369cd3ad143a7c45d396d0327f10d4a69
Nguyen Ba Son <44664162+N0sBa@users.noreply.github.com>
2021-03-08 12:59:30 +0700

    [fix] - update model (#177)
    * [config] - config International Collaboration's folder structure
    
    * [fix] - fix International Collaboration Views's folder structure
    
    * [fix] - fix International Collaboration's Views folder structure
    
    * [add] - add layout for International Collaboration moudle
    
    * [fix] - comment auto generate datatable with specified columns code in IC layout
    
    * [add] - add EntityFramework package in MANAGER project
    
    * [fix] - update model
    
    * [fix] - update model

--- ENTITIES/AcademicCollaboration.cs
+++ ENTITIES/AcademicCollaboration.cs
@@ -18,7 +18,6 @@ namespace ENTITIES
         public AcademicCollaboration()
         {
             this.CollaborationStatusHistories = new HashSet<CollaborationStatusHistory>();
-            this.Procedures = new HashSet<Procedure>();
         }
     
         public System.DateTime plan_study_start_date { get; set; }
@@ -28,16 +27,14 @@ namespace ENTITIES
         public int direction_id { get; set; }
         public int people_id { get; set; }
         public Nullable<bool> is_supported { get; set; }
-        public Nullable<int> mou_partner_scope_id { get; set; }
+        public Nullable<int> partner_scope_id { get; set; }
         public Nullable<System.DateTime> actual_study_start_date { get; set; }
         public Nullable<System.DateTime> actual_study_end_date { get; set; }
     
         public virtual Person Person { get; set; }
         public virtual Direction Direction { get; set; }
-        public virtual MOUPartnerScope MOUPartnerScope { get; set; }
+        public virtual PartnerScope PartnerScope { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<CollaborationStatusHistory> CollaborationStatusHistories { get; set; }
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<Procedure> Procedures { get; set; }
     }
 }
