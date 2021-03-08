a13672b46de6b13263f386c96be3f899921644f1
Đăng Dũng <47218583+Ctbom@users.noreply.github.com>
2021-03-08 21:15:56 +0700

    [add] fixing add MOU Function (#184)
    * [fix] remove redundant code MOU MOA
    
    * [add] fixing add MOU Function
    
    * Update MOURepo.cs
    
    * [fix] update CustomModel folder
    
    * [fix] import model

---
+++ ENTITIES/CustomModels/InternationalCollaboration/Collaboration/MemorandumOfUnderstanding/MOU/MOUAdd.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITIES.CustomModels.InternationalCollaboration.Collaboration.MemorandumOfUnderstanding.MOU
+{
+    public class MOUAdd
+    {
+        public MOUAdd() { }
+        public BasicInfo BasicInfo { get; set; }
+        public List<PartnerInfo> PartnerInfo { get; set; }
+    }
+}
