7d9c318a7870653ff27da821a875b03a4c2a34c9
Ctbom <dungpdhe130254@fpt.edu.vn>
2021-03-08 21:14:18 +0700

    [fix] import model

--- MANAGER/Controllers/InternationalCollaboration/Collaboration/MemorandumOfUnderstanding/MOUController.cs
+++ MANAGER/Controllers/InternationalCollaboration/Collaboration/MemorandumOfUnderstanding/MOUController.cs
@@ -1,4 +1,5 @@
 ﻿using BLL.InternationalCollaboration.Collaboration.MemorandumOfUnderstanding;
+using ENTITIES.CustomModels.InternationalCollaboration.Collaboration.MemorandumOfUnderstanding.MOU;
 using System;
 using System.Collections.Generic;
 using System.Linq;
