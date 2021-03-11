2224a036bce350d125763a97f0e25b06355c0072
thang1160 <35557579+thang1160@users.noreply.github.com>
2021-03-11 19:29:01 +0700

    [add] màn config google drive bên admin

---
+++ ADMIN/Controllers/SettingDriveController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ADMIN.Controllers
+{
+    public class SettingDriveController : Controller
+    {
+        // GET: SettingDrive
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
