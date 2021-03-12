385f3b6be82d5fa4c3c65713d5e850e0124b2555
duongpl <51127228+duongpl@users.noreply.github.com>
2021-03-12 11:27:03 +0700

    [fix] - Login Controller and setup SigninGoogle (#255)
    * Revert "[add] - create Scholarship controller and list view (#64)"
    
    This reverts commit 59dfffbb577380591aa2ceecb1ced2ba9429db57.
    
    * Revert "Revert "[add] - create Scholarship controller and list view (#64)""
    
    This reverts commit 5f0dddc69eb1566e030cea020be6a89d7c23881a.
    
    * Revert "[add] - create Scholarship controller and list view (#64)"
    
    This reverts commit 59dfffbb577380591aa2ceecb1ced2ba9429db57.
    
    * [fix] - AcademicActivity front-end and InternationalCollaboration layout
    
    * [fix] - fix AcademicActivity front-end
    
    * [fix,add] - fix AcademicActivity front-end and add Add,Edit,Delete in controller
    
    * Delete pre-commit
    
    * Create pre-commit
    
    * [fix,add] - fix AcademicActivity front-end and add Add,Edit,Delete in AcademicActivityController
    
    * [add - fix] - add CheckInController + front-end and link concern pages to each other
    
    * Revert "[add - fix] - add CheckInController + front-end and link concern pages to each other (#81)"
    
    This reverts commit 20654c6c76039352b7223cb774c1c0182dea36b2.
    
    * [fix] - resolve conflic
    
    * Update CheckInController.cs
    
    * [add] - add DetailOfAcademicActitivyController + front-end and add script ckeditor
    
    * Update DetailOfAcademicActivityController.cs
    
    * [fix] - fix DetailOfAcademicActivity front-end
    
    * [fix] - fix format date of datepicker
    
    * [fix] - front-end of DetailAcademicActivity and add control row for phase of activity
    
    * Update DetailOfAcademicActivityController.cs
    
    * [fix] - fix front-end AcademicActivity to match with manager convension
    
    * [add] - add front-end Detail/Co-organize partner
    
    * Update MOURepo.cs
    
    * .
    
    * Update MOURepo.cs
    
    * [fix] - fix front-end of RegisterForm and change components by manager convension
    
    * [add-fix] - add RegisterForm front-end and edit front-end to match manager convension
    
    * [add] - add LoginController and add Expense Academic Activity
    
    * [fix] - fix Academic Activity Expense
    
    * [fix] - front-end Academic Activity
    
    * [fix] - almost done Academic Activity Expense
    
    * [fix] -fix front-end Academic Activity Expense
    
    * [fix] - complete the detail of AA front-end
    
    * Update ResearchersController.cs
    
    * [fix] - complete Academic Activity Expense
    
    * [fix] - Login Controller and setup SigninGoogle
    
    * Update Login.cs

--- MANAGER/Controllers/AuthenticationAuthorization/AuthenController.cs
+++ MANAGER/Controllers/AuthenticationAuthorization/AuthenController.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ENTITIES;
+using BLL;
 
 namespace MANAGER.Controllers.AuthenticationAuthorization
 {
     public class AuthenController : Controller
     {
         // GET: Authen
+        private static BLL.Authen.Login authen = new BLL.Authen.Login();
         public ActionResult Login()
         {
             return View();
+        }
+        [HttpPost]
+        public JsonResult SigninGoogle(ENTITIES.CustomModels.Authen.Gmail user)
+        {
+            string url = authen.getAccount(user);
+            if (String.IsNullOrEmpty(url))
+            {
+                return Json("not allow", JsonRequestBehavior.AllowGet);
+            }
+            return Json(url, JsonRequestBehavior.AllowGet);
         }
     }
 }
