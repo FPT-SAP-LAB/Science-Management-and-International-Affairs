b330b9a8c8012dfe384113ea869ab2ec87b009a6
sonnh <yggdrasil2769@gmail.com>
2021-03-13 18:20:29 +0700

    [fix] - fix js code factor variable no used

--- MANAGER/Scripts/Custom_JS/InternationalCollaboration/kt_datetimepicker_3_and_range.js
+++ MANAGER/Scripts/Custom_JS/InternationalCollaboration/kt_datetimepicker_3_and_range.js
@@ -1,37 +1,17 @@
 ﻿//datepicker
-var KTBootstrapDatetimepicker = function () {
-    var baseDemos = function () {
-        $('.kt_datetimepicker_3').datetimepicker({
-            format: 'DD/MM/yyyy'
-        });
 
-    }
-    return {
-        // Public functions
-        init: function () {
-            baseDemos();
-        }
-    };
-}();
-
-var KTBootstrapDaterangepicker = function () {
-
-    // Private functions
-    var demos = function () {
-        // minimum setup
-        $('.kt_daterangepicker_1').daterangepicker({
-            buttonClasses: 'btn',
-            applyClass: 'btn-primary',
-            cancelClass: 'btn-secondary',
-            format: 'DD/MM/yyyy'
-        });
+$(document).ready(function () {
+    $('.kt_datetimepicker_3').datetimepicker({
+        format: 'DD/MM/yyyy'
+    });
 
 
-    }
-    return {
-        // public functions
-        init: function () {
-            demos();
-        }
-    };
-}();
+    $('.kt_daterangepicker_1').daterangepicker({
+        buttonClasses: 'btn',
+        applyClass: 'btn-primary',
+        cancelClass: 'btn-secondary',
+        format: 'DD/MM/yyyy'
+    });
+})
+
+
