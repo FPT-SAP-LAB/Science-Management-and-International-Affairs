5603290c1da7a450814edfe29a4c65f1c098d7c0
Đăng Dũng <47218583+Ctbom@users.noreply.github.com>
2021-03-16 22:59:02 +0700

    [add] add function backend MOU + MOA (#337)
    * [fix] remove redundant code MOU MOA
    
    * [add] fixing add MOU Function
    
    * Update MOURepo.cs
    
    * [fix] update CustomModel folder
    
    * [fix] import model
    
    * [add] add BackEnd code
    
    * [add] add BackEnd code
    
    * [fix] fix Add and Edit ExMOU code
    
    * [fix] fix Add and Edit ExMOU code
    
    * [add] add and fix function of MOU Module
    
    * [add] add and fix function of MOU Module
    
    * [fix] fixing old query MOU
    
    * Update MOARepo.cs
    
    * Update MOAController.cs
    
    * [fix] fixing old query MOU
    
    * [fix] fixing old query MOU
    
    * Update AcademicActivityRepo.cs
    
    * [add] add function backend MOU + MOA
    
    * [add] add function backend MOU + MOA
    
    * [fix] seperate files to Entities Project
    
    * Update MOABasicInfo.cs
    
    * Update PartnerMOARepo.cs
    
    * [fix] fix bug from CodeFactor

---
+++ ENTITIES/CustomModels/InternationalCollaboration/Collaboration/MemorandumOfAgreement/MOABasicInfo/CustomPartner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITIES.CustomModels.InternationalCollaboration.Collaboration.MemorandumOfAgreement.MOABasicInfo
+{
+    public class CustomPartner
+    {
+        public CustomPartner(int partner_id, string partner_name)
+        {
+            this.partner_id = partner_id;
+            this.partner_name = partner_name;
+        }
+        public CustomPartner() { }
+        public List<CustomScope> ListScopeExMOU { get; set; }
+        public int partner_id { get; set; }
+        public string partner_name { get; set; }
+    }
+}
