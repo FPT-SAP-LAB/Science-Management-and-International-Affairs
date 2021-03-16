35a735537459d83a85a1bce049a4f938daa0071c
Ctbom <dungpdhe130254@fpt.edu.vn>
2021-03-16 22:52:33 +0700

    Update MOABasicInfo.cs

--- ENTITIES/CustomModels/InternationalCollaboration/Collaboration/MemorandumOfAgreement/MOABasicInfo/MOABasicInfo.cs
+++ ENTITIES/CustomModels/InternationalCollaboration/Collaboration/MemorandumOfAgreement/MOABasicInfo/MOABasicInfo.cs
@@ -9,21 +9,21 @@ namespace ENTITIES.CustomModels.InternationalCollaboration.Collaboration.Memoran
     public class MOABasicInfo
     {
 
-            public MOABasicInfo() { }
-            public int moa_id { get; set; }
-            public string moa_code { get; set; }
-            public string evidence { get; set; }
-            public string scope_abbreviation { get; set; }
-            public string reason { get; set; }
-            public DateTime moa_end_date { get; set; }
-            public DateTime moa_start_date { get; set; }
-            public string moa_end_date_string { get; set; }
-            public string moa_start_date_string { get; set; }
-            public string office_abbreviation { get; set; }
-            public string mou_status_name { get; set; }
-            public int office_id { get; set; }
-            public int moa_status_id { get; set; }
-            public string moa_note { get; set; }
+        public MOABasicInfo() { }
+        public int moa_id { get; set; }
+        public string moa_code { get; set; }
+        public string evidence { get; set; }
+        public string scope_abbreviation { get; set; }
+        public string reason { get; set; }
+        public DateTime moa_end_date { get; set; }
+        public DateTime moa_start_date { get; set; }
+        public string moa_end_date_string { get; set; }
+        public string moa_start_date_string { get; set; }
+        public string office_abbreviation { get; set; }
+        public string mou_status_name { get; set; }
+        public int office_id { get; set; }
+        public int moa_status_id { get; set; }
+        public string moa_note { get; set; }
 
     }
 }
