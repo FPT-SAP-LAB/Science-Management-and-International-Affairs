f569364f38076509801d0addc7014589157ff154
hvu0163 <53392914+hvu0163@users.noreply.github.com>
2021-03-16 17:34:13 +0700

    [add] back-end

---
+++ BLL/ScienceManagement/Invention/InventionRepo.cs
@@ -0,0 +1,23 @@
+﻿using ENTITIES.CustomModels.ScienceManagement.Invention;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.ScienceManagement.Invention
+{
+    public class InventionRepo
+    {
+        readonly ScienceAndInternationalAffairsEntities db = new ScienceAndInternationalAffairsEntities();
+        public DetailInvention getDetail(string id)
+        {
+            DetailInvention item = new DetailInvention();
+            string sql = @"select i.*, it.name as 'type_name', ri.reward_type, ri.total_reward
+                            from [SM_ScientificProduct].Invention i join [SM_ScientificProduct].InventionType it on i.type_id = it.invention_type_id
+	                            join [SM_ScientificProduct].RequestInvention ri on i.invention_id = ri.invention_id
+                            where i.invention_id = @id";
+            return item;
+        }
+    }
+}
---
+++ ENTITIES/CustomModels/ScienceManagement/Invention/DetailInvention.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITIES.CustomModels.ScienceManagement.Invention
+{
+    public class DetailInvention : ENTITIES.Invention
+    {
+        public string type_name { get; set; }
+        public string reward_type { get; set; }
+        public string total_reward { get; set; }
+    }
+}
