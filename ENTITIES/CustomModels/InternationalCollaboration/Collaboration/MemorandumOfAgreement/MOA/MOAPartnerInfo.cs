9649ba5a8d09e034588e892f39247f05e5126769
Đăng Dũng <47218583+Ctbom@users.noreply.github.com>
2021-03-17 15:21:13 +0700

    [fix] changed files to ENTITIES project (#355)
    * [fix] remove redundant code MOU MOA
    
    * [add] fixing add MOU Function
    
    * Update MOURepo.cs
    
    * [fix] update CustomModel folder
    
    * [fix] import model
    
    * [add] add BackEnd code
    
    * [add] add BackEnd code
    
    * [fix] fix Add and Edit ExMOU code
    
    * [fix] fix Add and Edit ExMOU code
    
    * [add] add and fix function of MOU Module
    
    * [add] add and fix function of MOU Module
    
    * [fix] fixing old query MOU
    
    * Update MOARepo.cs
    
    * Update MOAController.cs
    
    * [fix] fixing old query MOU
    
    * [fix] fixing old query MOU
    
    * Update AcademicActivityRepo.cs
    
    * [add] add function backend MOU + MOA
    
    * [add] add function backend MOU + MOA
    
    * [fix] seperate files to Entities Project
    
    * Update MOABasicInfo.cs
    
    * Update PartnerMOARepo.cs
    
    * [fix] fix bug from CodeFactor
    
    * Update PartnerMOAController.cs
    
    * [fix] changed files to ENTITIES project

---
+++ ENTITIES/CustomModels/InternationalCollaboration/Collaboration/MemorandumOfAgreement/MOA/MOAPartnerInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITIES.CustomModels.InternationalCollaboration.Collaboration.MemorandumOfAgreement.MOA
+{
+    public class MOAPartnerInfo
+    {
+        public string partnername_add { get; set; }
+        public string sign_date_moa_add { get; set; }
+        public List<int> coop_scope_add { get; set; }
+        public int partner_id { get; set; }
+    }
+}
---
+++ ENTITIES/CustomModels/InternationalCollaboration/Collaboration/MemorandumOfAgreement/MOAPartner/ScopeAndStartDate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITIES.CustomModels.InternationalCollaboration.Collaboration.MemorandumOfAgreement.MOAPartner
+{
+    public class ScopeAndStartDate
+    {
+        public int scope_id { get; set; }
+        public DateTime moa_start_date { get; set; }
+    }
+}
