947491d8d60acbd870df3d2c926dc474a870cd62
sonnh <yggdrasil2769@gmail.com>
2021-03-17 09:12:08 +0700

    [add] - masterdata and controller

---
+++ BLL/InternationalCollaboration/MasterData/AcademicActivityExpenseTypeRepo.cs
@@ -0,0 +1,21 @@
+﻿using ENTITIES;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.InternationalCollaboration.MasterData
+{
+    
+    class AcademicActivityExpenseTypeRepo
+    {
+        readonly ScienceAndInternationalAffairsEntities db = new ScienceAndInternationalAffairsEntities();
+
+        public HashSet<ENTITIES.ActivityExpenseType> activityExpenseTypes()
+        {
+            HashSet<ActivityExpenseType> activityExpenseTypes = db.ActivityExpenseTypes.ToHashSet();
+            return activityExpenseTypes;
+        }
+    }
+}
