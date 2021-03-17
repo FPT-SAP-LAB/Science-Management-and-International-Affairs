271bd74c3a047c3c1cdab06303de6fc854fcfb55
hvu0163 <53392914+hvu0163@users.noreply.github.com>
2021-03-17 21:49:21 +0700

    [fix] front-end (#359)
    * [add] back-end
    
    * Update InventionRepo.cs
    
    * [add] back-end
    
    * [fix] front-end so trich dan
    
    * [add] back-end khen thuong trich dan
    
    * Update PartialViewController.cs
    
    * [fix] front-end

--- GUEST/Scripts/PartialViewScript.js
+++ GUEST/Scripts/PartialViewScript.js
@@ -36,11 +36,11 @@ donvife = {
 
 $("#add_author_save").click(function () {
     ckfe = $("#ckfe").val()
-    add_author_workplace = $("#add_author_workplace").val()
+    add_author_workplace = $("#ckfe").val()
     add_author_msnv = $("#add_author_msnv").val()
     add_author_name = $("#add_author_name").val()
-    add_author_title = $("#add_author_title").val()
-    add_author_contractType = $("#add_author_contractType").val()
+    add_author_title = $("#add_author_title option:selected").text()
+    add_author_contractType = $("#add_author_contractType option:selected").text()
     add_author_cmnd = $("#add_author_cmnd").val()
     add_author_tax = $("#add_author_tax").val()
     add_author_bank = $("#add_author_bank").val()
@@ -50,7 +50,7 @@ $("#add_author_save").click(function () {
     row_id = new Date().getTime()
     $("#tacgia_body").append("\
                 <tr id='"+ row_id + "'>"
-        + "<td>" + donvife[ckfe] + "</td>"
+        + "<td>" + add_author_workplace + "</td>"
         + "<td>" + add_author_msnv + "</td>"
         + "<td>" + add_author_name + "</td>"
         + "<td>" + add_author_title + "</td>"
@@ -79,4 +79,112 @@ $(document).ready(function () {
     if (numberOfAuthors > 0) {
         $(".tacgia").show()
     }
+});
+
+"use strict";
+
+// Class definition
+var KTUppy = function () {
+    const Tus = Uppy.Tus;
+    const ProgressBar = Uppy.ProgressBar;
+    const StatusBar = Uppy.StatusBar;
+    const FileInput = Uppy.FileInput;
+    const Informer = Uppy.Informer;
+
+    // to get uppy companions working, please refer to the official documentation here: https://uppy.io/docs/companion/
+    const Dashboard = Uppy.Dashboard;
+    const Dropbox = Uppy.Dropbox;
+    const GoogleDrive = Uppy.GoogleDrive;
+    const Instagram = Uppy.Instagram;
+    const Webcam = Uppy.Webcam;
+
+    // Private functions
+
+    var initUppy5 = function () {
+        // Uppy variables
+        // For more info refer: https://uppy.io/
+        var elemId = 'kt_uppy_5';
+        var id = '#' + elemId;
+        var $statusBar = $(id + ' .uppy-status');
+        var $uploadedList = $(id + ' .uppy-list');
+        var timeout;
+
+        var uppyMin = Uppy.Core({
+            debug: true,
+            autoProceed: true,
+            showProgressDetails: true,
+            restrictions: {
+                maxFileSize: 1000000, // 1mb
+                maxNumberOfFiles: 5,
+                minNumberOfFiles: 1
+            }
+        });
+
+        uppyMin.use(FileInput, { target: id + ' .uppy-wrapper', pretty: false });
+        uppyMin.use(Informer, { target: id + ' .uppy-informer' });
+
+        // demo file upload server
+        uppyMin.use(Tus, { endpoint: 'https://master.tus.io/files/' });
+        uppyMin.use(StatusBar, {
+            target: id + ' .uppy-status',
+            hideUploadButton: true,
+            hideAfterFinish: false
+        });
+
+        $(id + ' .uppy-FileInput-input').addClass('uppy-input-control').attr('id', elemId + '_input_control');
+        $(id + ' .uppy-FileInput-container').append('<label class="uppy-input-label btn btn-light-primary btn-sm btn-bold" for="' + (elemId + '_input_control') + '">Attach files</label>');
+
+        var $fileLabel = $(id + ' .uppy-input-label');
+
+        uppyMin.on('upload', function (data) {
+            $fileLabel.text("Uploading...");
+            $statusBar.addClass('uppy-status-ongoing');
+            $statusBar.removeClass('uppy-status-hidden');
+            clearTimeout(timeout);
+        });
+
+        uppyMin.on('complete', function (file) {
+            $.each(file.successful, function (index, value) {
+                var sizeLabel = "bytes";
+                var filesize = value.size;
+                if (filesize > 1024) {
+                    filesize = filesize / 1024;
+                    sizeLabel = "kb";
+
+                    if (filesize > 1024) {
+                        filesize = filesize / 1024;
+                        sizeLabel = "MB";
+                    }
+                }
+                var uploadListHtml = '<div class="uppy-list-item" data-id="' + value.id + '"><div class="uppy-list-label">' + value.name + ' (' + Math.round(filesize, 2) + ' ' + sizeLabel + ')</div><span class="uppy-list-remove" data-id="' + value.id + '"><i class="flaticon2-cancel-music"></i></span></div>';
+                $uploadedList.append(uploadListHtml);
+            });
+
+            $fileLabel.text("Add more files");
+
+            $statusBar.addClass('uppy-status-hidden');
+            $statusBar.removeClass('uppy-status-ongoing');
+        });
+
+        $(document).on('click', id + ' .uppy-list .uppy-list-remove', function () {
+            var itemId = $(this).attr('data-id');
+            uppyMin.removeFile(itemId);
+            $(id + ' .uppy-list-item[data-id="' + itemId + '"').remove();
+        });
+    }
+
+
+    return {
+        // public functions
+        init: function () {
+            initUppy5();
+            setTimeout(function () {
+
+            }, 2000);
+        }
+    };
+}();
+
+KTUtil.ready(function () {
+    KTUppy.init();
 });
