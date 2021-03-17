2f851223fab42a1ff30ceb22c95440ee68d31848
N0SBa <nguyenbason.fpt04041999@gmail.com>
2021-03-17 23:16:09 +0700

    [edit] - update model & academic activity type

--- ENTITIES/CustomModels/AlertModal.cs
+++ ENTITIES/CustomModels/AlertModal.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace ENTITIES.CustomModels
 {
-    public class AlertModal
+    public class AlertModal<T>
     {
         public bool success { get; set; }
         public string title { get; set; }
         public string content { get; set; }
+        public T obj { get; set; }
+
+        public AlertModal(T obj, bool success, string title, string content)
+        {
+            this.obj = obj;
+            this.success = success;
+            this.title = title;
+            this.content = content;
+        }
     }
 }
--- MANAGER/Controllers/InternationalCollaboration/MasterData/AcademicActivityTypeController.cs
+++ MANAGER/Controllers/InternationalCollaboration/MasterData/AcademicActivityTypeController.cs
@@ -28,13 +28,68 @@ namespace MANAGER.Controllers.InternationalCollaboration.MasterData
             try
             {
                 BaseDatatable baseDatatable = new BaseDatatable(Request);
-                List<AcademicActivityType> academicActivityTypes = academicActivityTypeRepo.getlistAcademicActivityType(baseDatatable);
-                return Json(new { success = true, data = academicActivityTypes }, JsonRequestBehavior.AllowGet);
+                BaseServerSideData<AcademicActivityType> academicActivityTypes = academicActivityTypeRepo.getlistAcademicActivityType(baseDatatable);
+                return Json(new { success = true, data = academicActivityTypes.Data, draw = Request["draw"], 
+                    recordsTotal = academicActivityTypes.RecordsTotal, 
+                    recordsFiltered = academicActivityTypes.RecordsTotal }, JsonRequestBehavior.AllowGet);
             }
             catch (Exception e)
             {
                 return Json(new { success = false });
             }
         }
+
+        [HttpPost]
+        public ActionResult addAcademicActivityType(string academic_activity_type_name)
+        {
+            try
+            {
+                AlertModal<AcademicActivityType> alertModal = academicActivityTypeRepo.addAcademicActivityType(academic_activity_type_name);
+                return Json(new { alertModal.success, alertModal.title, alertModal.content });
+            } catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        [HttpPost]
+        public ActionResult getAcademicActivityType(int academic_activity_type_id)
+        {
+            try
+            {
+                AlertModal<AcademicActivityType> alertModal = academicActivityTypeRepo.getAcademicActivityType(academic_activity_type_id);
+                return Json(new { alertModal.obj, alertModal.success, alertModal.title, alertModal.content });
+            } catch(Exception e)
+            {
+                throw e;
+            }
+        }
+
+        [HttpPost]
+        public ActionResult editAcademicActivty(int academic_activity_type_id, string academic_activity_type_name)
+        {
+            try
+            {
+                AlertModal<AcademicActivityType> alertModal = academicActivityTypeRepo.editAcademicActivityType(academic_activity_type_id, academic_activity_type_name);
+                return Json(new { alertModal.success, alertModal.title, alertModal.content });
+            } catch(Exception e)
+            {
+                throw e;
+            }
+        }
+
+        [HttpPost]
+        public ActionResult deleteAcademicActivityType(int academic_activity_type_id)
+        {
+            try
+            {
+                AlertModal<AcademicActivityType> alertModal = academicActivityTypeRepo.deleteAcademicActivityType(academic_activity_type_id);
+                return Json(new { alertModal.success, alertModal.title, alertModal.content });
+            } catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
     }
 }
