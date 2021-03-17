d9f3ce511cbef04282a0fd10ed1d50b6cef2214e
thang1160 <35557579+thang1160@users.noreply.github.com>
2021-03-17 10:23:47 +0700

    [edit] Chuyển qua sử dụng generic class cho datatable

---
+++ ENTITIES/CustomModels/ScienceManagement/Conference/ConferenceIndex.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITIES.CustomModels.ScienceManagement.Conference
+{
+    public class ConferenceIndex
+    {
+        public int RequestID { get; set; }
+        public int RowNumber { get; set; }
+        public string PaperName { get; set; }
+        public string ConferenceName { get; set; }
+        public DateTime Date { get; set; }
+        public string CreatedDate { get; set; }
+        public string StatusName { get; set; }
+        public int StatusID { get; set; }
+        public string FullName { get; set; }
+    }
+}
