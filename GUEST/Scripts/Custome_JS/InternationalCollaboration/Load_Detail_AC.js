b7a739870a48ccae8f366e3242f47f424370ee59
sonnh <yggdrasil2769@gmail.com>
2021-03-18 13:25:32 +0700

    [add] - add view detail of program and procedure

---
+++ GUEST/Scripts/Custome_JS/InternationalCollaboration/Load_Detail_AC.js
@@ -0,0 +1,8 @@
+﻿function load_program_detail(id, type_program) {
+    var url = '/AcademicCollaboration/Program_Detail?id=' + id + '&type_program=' + type_program
+    window.location.href = url
+}
+function load_procedure_detail(id, type_procedure) {
+    var url = '/AcademicCollaboration/Procedure_Detail?id=' + id + '&type_procedure=' + type_procedure
+    window.location.href = url
+}
