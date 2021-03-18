79147f6af869053226a6ef23b52dc499dcf79d17
duongpl <51127228+duongpl@users.noreply.github.com>
2021-03-18 17:32:49 +0700

    [fix] - code factor (complexity) + academic activity bankend and checkin backend (remove allowget from post method) (#368)
    * Revert "[add] - create Scholarship controller and list view (#64)"
    
    This reverts commit 59dfffbb577380591aa2ceecb1ced2ba9429db57.
    
    * Revert "Revert "[add] - create Scholarship controller and list view (#64)""
    
    This reverts commit 5f0dddc69eb1566e030cea020be6a89d7c23881a.
    
    * Revert "[add] - create Scholarship controller and list view (#64)"
    
    This reverts commit 59dfffbb577380591aa2ceecb1ced2ba9429db57.
    
    * [fix] - AcademicActivity front-end and InternationalCollaboration layout
    
    * [fix] - fix AcademicActivity front-end
    
    * [fix,add] - fix AcademicActivity front-end and add Add,Edit,Delete in controller
    
    * Delete pre-commit
    
    * Create pre-commit
    
    * [fix,add] - fix AcademicActivity front-end and add Add,Edit,Delete in AcademicActivityController
    
    * [add - fix] - add CheckInController + front-end and link concern pages to each other
    
    * Revert "[add - fix] - add CheckInController + front-end and link concern pages to each other (#81)"
    
    This reverts commit 20654c6c76039352b7223cb774c1c0182dea36b2.
    
    * [fix] - resolve conflic
    
    * Update CheckInController.cs
    
    * [add] - add DetailOfAcademicActitivyController + front-end and add script ckeditor
    
    * Update DetailOfAcademicActivityController.cs
    
    * [fix] - fix DetailOfAcademicActivity front-end
    
    * [fix] - fix format date of datepicker
    
    * [fix] - front-end of DetailAcademicActivity and add control row for phase of activity
    
    * Update DetailOfAcademicActivityController.cs
    
    * [fix] - fix front-end AcademicActivity to match with manager convension
    
    * [add] - add front-end Detail/Co-organize partner
    
    * Update MOURepo.cs
    
    * .
    
    * Update MOURepo.cs
    
    * [fix] - fix front-end of RegisterForm and change components by manager convension
    
    * [add-fix] - add RegisterForm front-end and edit front-end to match manager convension
    
    * [add] - add LoginController and add Expense Academic Activity
    
    * [fix] - fix Academic Activity Expense
    
    * [fix] - front-end Academic Activity
    
    * [fix] - almost done Academic Activity Expense
    
    * [fix] -fix front-end Academic Activity Expense
    
    * [fix] - complete the detail of AA front-end
    
    * Update ResearchersController.cs
    
    * [fix] - complete Academic Activity Expense
    
    * [fix] - Login Controller and setup SigninGoogle
    
    * Update Login.cs
    
    * [add] - List of academic activity for guest
    
    * fix
    
    * [add] - detail of academic activity for guest
    
    * [add config] - layout and controller for management system
    
    * [add] - code backend for academic activity for manager
    
    * [fix] - code facter
    
    * [fix] - backend of academic activity for manager
    
    * Update AcademicActivityController.cs
    
    * [fix] - code factor
    
    * Update AcademicActivityRepo.cs
    
    * [fix add] - fix academic activity and complete login via google account
    
    * update
    
    * Update Web.config
    
    * [fix - add] management user and fix backend list academic activity
    
    * update
    
    * [conflict] fix conflict
    
    * [fix] - fix backend of academic activity manager
    
    * [fix] - code factor
    
    * [fix] - code factor
    
    * [add] - checkin backend , fix convention
    
    * Update CheckInRepo.cs
    
    * [add] - checkin backend , fix convention
    
    * Update CheckInRepo.cs
    
    * [fix] - code factor
    
    * Update List.cshtml
    
    * [fix] - academic activity backend and checkin backend, remove allowget from post method
    
    * [fix] - code factor (complexity)
    
    * [add] - frontend and backend for right management
    
    * Update RightRepo.cs
    
    * [fix] - backend right
    
    * [fix] - code factor (complexity)
    
    * Update AcademicActivityRepo.cs
    
    Co-authored-by: Quoc Tuan <39939107+quoctuanck99@users.noreply.github.com>
    Co-authored-by: thang1160 <35557579+thang1160@users.noreply.github.com>

---
+++ BLL/Admin/RightRepo.cs
@@ -0,0 +1,115 @@
+﻿using ENTITIES;
+using ENTITIES.CustomModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace BLL.Admin
+{
+    public class RightRepo
+    {
+        readonly ScienceAndInternationalAffairsEntities db = new ScienceAndInternationalAffairsEntities();
+        public BaseServerSideData<infoRight> getRights(BaseDatatable baseDatatable)
+        {
+            try
+            {
+                db.Configuration.LazyLoadingEnabled = false;
+                string sql = @"select r.right_id,r.right_name,m.module_name,r.group_id
+                                from General.[Right] r inner join General.Module m on r.module_id = m.module_id 
+                                ORDER BY " + baseDatatable.SortColumnName + " " + baseDatatable.SortDirection +
+                                " OFFSET " + baseDatatable.Start + " ROWS FETCH NEXT " + baseDatatable.Length + " ROWS ONLY";
+                int recordTotal = db.Database.SqlQuery<int>("select count(*) from General.[Right] r inner join General.Module m on r.module_id = m.module_id").FirstOrDefault();
+                List<infoRight> data = db.Database.SqlQuery<infoRight>(sql).ToList();
+                return new BaseServerSideData<infoRight>(data, recordTotal);
+            }
+            catch (Exception e)
+            {
+                return new BaseServerSideData<infoRight>(new List<infoRight>(), 0);
+            }
+        }
+        public List<Module> getModules()
+        {
+            try
+            {
+                List<Module> data = db.Modules.ToList();
+                return data;
+            }
+            catch (Exception e)
+            {
+                return new List<Module>();
+            }
+        }
+        public Right GetBaseRight(int right_id)
+        {
+            try
+            {
+                Right data = db.Rights.Find(right_id);
+                return data;
+            }
+            catch (Exception e)
+            {
+                return new Right();
+            }
+        }
+        public bool add(baseRight obj)
+        {
+            try
+            {
+                db.Rights.Add(new Right
+                {
+                    right_name = obj.right_name,
+                    group_id = obj.group_id,
+                    module_id = int.Parse(obj.module_name)
+                });
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+        public bool edit(infoRight obj)
+        {
+            try
+            {
+                Right r = db.Rights.Find(obj.right_id);
+                r.right_name = obj.right_name;
+                r.module_id = int.Parse(obj.module_name);
+                r.group_id = obj.group_id;
+                db.Entry(r).State = EntityState.Modified;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+        public bool delete(int right_id)
+        {
+            try
+            {
+                Right r = db.Rights.Find(right_id);
+                db.Rights.Remove(r);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+        public class infoRight : baseRight
+        {
+            public int right_id { get; set; }
+        }
+        public class baseRight
+        {
+            public string right_name { get; set; }
+            public string module_name { get; set; }
+            public int group_id { get; set; }
+        }
+    }
+}
