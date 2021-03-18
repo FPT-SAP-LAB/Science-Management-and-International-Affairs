b418f8a654624ed3f61be16a474f047f9ba49ea9
sonnh <yggdrasil2769@gmail.com>
2021-03-18 13:39:04 +0700

    [fix] - create class for js load

--- GUEST/Scripts/Custome_JS/InternationalCollaboration/Load_Detail_AC.js
+++ GUEST/Scripts/Custome_JS/InternationalCollaboration/Load_Detail_AC.js
@@ -1,8 +1,14 @@
-﻿function load_program_detail(id, type_program) {
-    var url = '/AcademicCollaboration/Program_Detail?id=' + id + '&type_program=' + type_program
-    window.location.href = url
+﻿
+
+class Load_detail {
+    load_procedure_detail(id, type_procedure) {
+        var url = '/AcademicCollaboration/Procedure_Detail?id=' + id + '&type_procedure=' + type_procedure
+        window.location.href = url
+    }
+
+    load_program_detail(id, type_program) {
+        var url = '/AcademicCollaboration/Program_Detail?id=' + id + '&type_program=' + type_program
+        window.location.href = url
+    }
 }
-function load_procedure_detail(id, type_procedure) {
-    var url = '/AcademicCollaboration/Procedure_Detail?id=' + id + '&type_procedure=' + type_procedure
-    window.location.href = url
-}
+var load_detail = new Load_detail()
