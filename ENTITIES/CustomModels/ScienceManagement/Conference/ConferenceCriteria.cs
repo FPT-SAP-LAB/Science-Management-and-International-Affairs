a078e9c9459227ee48db18aa4438a74d246dd1b9
thang1160 <35557579+thang1160@users.noreply.github.com>
2021-03-22 19:41:54 +0700

    [edit] lấy dữ liệu tiêu chí chi tiết đề nghị hội nghị GUEST (#420)

---
+++ ENTITIES/CustomModels/ScienceManagement/Conference/ConferenceCriteria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITIES.CustomModels.ScienceManagement.Conference
+{
+    public class ConferenceCriteria
+    {
+        public int CriteriaID { get; set; }
+        public string CriteriaName { get; set; }
+        public bool IsAccepted { get; set; }
+    }
+}
