1bfa3143f332a66e48ce535355abaca7c5128220
N0SBa <nguyenbason.fpt04041999@gmail.com>
2021-03-23 00:38:13 +0700

    [add] - add more function in Long-term Going case Academic Collaboration

--- MANAGER/Controllers/InternationalCollaboration/AcademicCollaboration/AcademicCollaborationController.cs
+++ MANAGER/Controllers/InternationalCollaboration/AcademicCollaboration/AcademicCollaborationController.cs
@@ -1,5 +1,7 @@
 ﻿using BLL.InternationalCollaboration.AcademicCollaboration;
+using ENTITIES;
 using ENTITIES.CustomModels;
+using ENTITIES.CustomModels.InternationalCollaboration;
 using ENTITIES.CustomModels.InternationalCollaboration.AcademicCollaborationEntities;
 using System;
 using System.Collections.Generic;
@@ -18,15 +20,15 @@ namespace MANAGER.Controllers.InternationalCollaboration.AcademicCollaboration
         public ActionResult Longterm_List()
         {
             ViewBag.title = "DANH SÁCH ĐÀO TẠO SAU ĐẠI HỌC";
-            //list for searching
-            //country
-            ViewBag.countries = academicCollaborationRepo.countries();
-            //year 
-            ViewBag.yearSearching = academicCollaborationRepo.yearSearching();
-            //office
-            ViewBag.offices = academicCollaborationRepo.offices();
-            //partner
-            ViewBag.partners = academicCollaborationRepo.partners();
+            ////list for searching
+            ////country
+            //ViewBag.countries = academicCollaborationRepo.countries();
+            ////year 
+            //ViewBag.yearSearching = academicCollaborationRepo.yearSearching();
+            ////office
+            //ViewBag.offices = academicCollaborationRepo.offices();
+            ////partner
+            //ViewBag.partners = academicCollaborationRepo.partners();
             return View();
         }
 
@@ -34,7 +36,7 @@ namespace MANAGER.Controllers.InternationalCollaboration.AcademicCollaboration
         public ActionResult getListAcademicCollaboration(int direction, int collab_type_id, ObjectSearching_AcademicCollaboration obj_searching)
         {
             BaseDatatable baseDatatable = new BaseDatatable(Request);
-            BaseServerSideData<AcademicCollaboration_Ext> baseServerSideData = academicCollaborationRepo.academicCollaborations(direction, collab_type_id, obj_searching);
+            BaseServerSideData<AcademicCollaboration_Ext> baseServerSideData = academicCollaborationRepo.academicCollaborations(direction, collab_type_id, obj_searching, baseDatatable);
             return Json(new
             {
                 success = true,
@@ -43,6 +45,79 @@ namespace MANAGER.Controllers.InternationalCollaboration.AcademicCollaboration
                 recordsTotal = baseServerSideData.RecordsTotal,
                 recordsFiltered = baseServerSideData.RecordsTotal
             }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult getCountries(string country_name)
+        {
+            AlertModal<List<Country>> alertModal = academicCollaborationRepo.countries(country_name);
+            return Json(new { alertModal.obj, alertModal.success, alertModal.title, alertModal.content }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult getYears()
+        {
+            AlertModal<YearSearching> alertModal = academicCollaborationRepo.yearSearching();
+            return Json(new { alertModal.obj, alertModal.success, alertModal.title, alertModal.content }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult getOffices(string office_name)
+        {
+            AlertModal<List<Office>> alertModal = academicCollaborationRepo.offices(office_name);
+            return Json(new { alertModal.obj, alertModal.success, alertModal.title, alertModal.content }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult getPartners(string partner_name)
+        {
+            AlertModal<List<AcademicCollaborationPartner_Ext>> alertModal = academicCollaborationRepo.partners(partner_name);
+            return Json(new { alertModal.obj, alertModal.success, alertModal.title, alertModal.content }, JsonRequestBehavior.AllowGet);
+        }
+
+        //add person
+        [HttpGet]
+        public ActionResult getPeople(string person_name)
+        {
+            AlertModal<List<AcademicCollaborationPerson_Ext>> alertModal = academicCollaborationRepo.people(person_name);
+            return Json(new { alertModal.obj, alertModal.success, alertModal.title, alertModal.content }, JsonRequestBehavior.AllowGet);
+        }
+        
+        [HttpGet]
+        public ActionResult getCollabScopes()
+        {
+            AlertModal<List<CollaborationScope>> alertModal = academicCollaborationRepo.collaborationScopes();
+            return Json(new { alertModal.obj, alertModal.success, alertModal.title, alertModal.content }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult getAcadCollabStatus(int status_type_specific)
+        {
+            AlertModal<List<AcademicCollaborationStatu>> alertModal = academicCollaborationRepo.academicCollaborationStatus(status_type_specific);
+            return Json(new { alertModal.obj, alertModal.success, alertModal.title, alertModal.content }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult checkPerson(int people_id, string people_name)
+        {
+            AlertModal<AcademicCollaborationPerson_Ext> alertModal = academicCollaborationRepo.person(people_id, people_name);
+            return Json(new { alertModal.obj, alertModal.success, alertModal.title, alertModal.content });
+        }
+
+        [HttpPost]
+        public ActionResult checkPartner(int partner_id, string partner_name)
+        {
+            AlertModal<AcademicCollaborationPartner_Ext> alertModal = academicCollaborationRepo.partner(partner_id, partner_name);
+            return Json(new { alertModal.obj, alertModal.success, alertModal.title, alertModal.content });
+        }
+
+        [HttpPost]
+        public ActionResult addPerson(Person person)
+        {
+            
+
+
+            return Json(new { });
         }
 
         public ActionResult Delete_Longterm(string id)
