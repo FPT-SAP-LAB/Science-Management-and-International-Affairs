60a2d6abdbeff9cd84cecdad1f187db099eb8271
hvu0163 <53392914+hvu0163@users.noreply.github.com>
2021-03-23 18:43:48 +0700

    Update JSUpload.js

--- GUEST/Scripts/JSUpload.js
+++ GUEST/Scripts/JSUpload.js
@@ -48,7 +48,7 @@
 
         var $fileLabel = $(id + ' .uppy-input-label');
 
-        uppy1.on('upload', function (data) {
+        uppy1.on('upload', function () {
             $fileLabel.text("Uploading...");
             $statusBar.addClass('uppy-status-ongoing');
             $statusBar.removeClass('uppy-status-hidden');
