e8cbd3749b4385a84ccb9c5570cee0aec49bae00
duongpl <shidoundie@gmail.com>
2021-03-23 22:06:50 +0700

    [add fix] add backend for academic activity content and fix login guest module

--- BLL/InternationalCollaboration/AcademicActivity/DetailOfAcademicActivityRepo.cs
+++ BLL/InternationalCollaboration/AcademicActivity/DetailOfAcademicActivityRepo.cs
@@ -10,7 +10,7 @@ namespace BLL.InternationalCollaboration.AcademicActivity
 {
     public class DetailOfAcademicActivityRepo
     {
-        readonly ScienceAndInternationalAffairsEntities db = new ScienceAndInternationalAffairsEntities();
+        ScienceAndInternationalAffairsEntities db = new ScienceAndInternationalAffairsEntities();
         public baseDetail getDetail(int language_id, int activity_id)
         {
             try
@@ -24,9 +24,46 @@ namespace BLL.InternationalCollaboration.AcademicActivity
                                     WHERE al.language_id = @language_id AND [aa].activity_id = @activity_id and ai.main_article = 1";
                 baseDetail data = db.Database.SqlQuery<baseDetail>(sql, new SqlParameter("language_id", language_id),
                                                                         new SqlParameter("activity_id", activity_id)).FirstOrDefault();
-                data.from = changeFormatDate(data.from);
-                data.to = changeFormatDate(data.to);
-                return data;
+                if (data != null)
+                {
+                    data.from = changeFormatDate(data.from);
+                    data.to = changeFormatDate(data.to);
+                    return data;
+                }
+                else
+                {
+                    using (DbContextTransaction transaction = db.Database.BeginTransaction())
+                    {
+                        try
+                        {
+                            db.AcademicActivityLanguages.Add(new AcademicActivityLanguage
+                            {
+                                activity_id = activity_id,
+                                language_id = language_id,
+                                location = String.Empty
+                            });
+                            db.SaveChanges();
+                            ActivityInfo ai = db.ActivityInfoes.Where(x => x.activity_id == activity_id && x.main_article == true).FirstOrDefault();
+                            db.ArticleVersions.Add(new ArticleVersion
+                            {
+                                publish_time = DateTime.Now,
+                                version_title = String.Empty,
+                                article_content = String.Empty,
+                                article_id = ai.article_id,
+                                language_id = language_id
+                            });
+                            db.SaveChanges();
+                            transaction.Commit();
+                        }
+                        catch (Exception e)
+                        {
+                            transaction.Rollback();
+                        }
+                    }
+                    data = db.Database.SqlQuery<baseDetail>(sql, new SqlParameter("language_id", language_id),
+                                                                        new SqlParameter("activity_id", activity_id)).FirstOrDefault();
+                    return data;
+                }
             }
             catch (Exception e)
             {
@@ -45,11 +82,29 @@ namespace BLL.InternationalCollaboration.AcademicActivity
                 string sql = @"SELECT ai.article_id, av.version_title, av.article_content, al.language_id
                                 FROM SMIA_AcademicActivity.AcademicActivity aa inner join SMIA_AcademicActivity.AcademicActivityLanguage al 
                                 on aa.activity_id = al.activity_id inner join SMIA_AcademicActivity.ActivityInfo ai
-                                on ai.activity_id = aa.activity_id and ai.main_article = 1 inner join IA_Article.Article ar
+                                on ai.activity_id = aa.activity_id inner join IA_Article.Article ar
                                 on ar.article_id = ai.article_id inner join IA_Article.ArticleVersion av
                                 on av.article_id = ai.article_id and al.language_id = av.language_id
                                 WHERE al.language_id = @language_id  AND [aa].activity_id = @activity_id and ai.main_article = 0";
                 List<subContent> data = db.Database.SqlQuery<subContent>(sql, new SqlParameter("language_id", language_id),
+                                                                              new SqlParameter("activity_id", activity_id)).ToList();
+                if(data.Count == 0)
+                {
+                    List<ActivityInfo> listAI = db.ActivityInfoes.Where(x => x.activity_id == activity_id && x.main_article == false).ToList();
+                    foreach (ActivityInfo ai in listAI)
+                    {
+                        db.ArticleVersions.Add(new ArticleVersion
+                        {
+                            article_id = ai.article_id,
+                            language_id = language_id,
+                            version_title = String.Empty,
+                            article_content = String.Empty,
+                            publish_time = DateTime.Now
+                        });
+                    }
+                    db.SaveChanges();
+                }
+                data = db.Database.SqlQuery<subContent>(sql, new SqlParameter("language_id", language_id),
                                                                               new SqlParameter("activity_id", activity_id)).ToList();
                 return data;
             }
@@ -70,6 +125,89 @@ namespace BLL.InternationalCollaboration.AcademicActivity
                 return new List<AcademicActivityType>();
             }
         }
+        public bool updateDetail(InfoSumDetail data, Authen.LoginRepo.User u)
+        {
+            using (DbContextTransaction transaction = db.Database.BeginTransaction())
+            {
+                try
+                {
+                    AcademicActivityRepo aaRepo = new AcademicActivityRepo();
+                    infoDetail detail = data.infoDetail;
+                    bool res = aaRepo.updateBaseAA(detail.activity_id, detail.activity_type_id, detail.activity_name, detail.location, detail.from, detail.to, detail.language_id);
+                    ActivityInfo ai = db.ActivityInfoes.Where(x => x.activity_id == detail.activity_id && x.main_article == true).FirstOrDefault();
+                    ArticleVersion av = db.ArticleVersions.Where(x => x.article_id == ai.article_id && x.language_id == detail.language_id).FirstOrDefault();
+                    av.article_content = detail.article_content;
+                    db.Entry(av).State = EntityState.Modified;
+                    db.SaveChanges();
+                    updateListSubContent(data.subContent, detail.language_id, detail.activity_id, u, detail.article_status_id);
+                    db.SaveChanges();
+                    transaction.Commit();
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    transaction.Rollback();
+                    return false;
+                }
+            }
+        }
+        public void updateListSubContent(List<subContent> data, int language_id, int activity_id, Authen.LoginRepo.User u, int article_status_id)
+        {
+            List<subContent> old = getSubContents(language_id, activity_id);
+            List<int> old_id = old.Select(x => x.article_id).ToList();
+            foreach (subContent x in data)
+            {
+                if (!old_id.Contains(x.article_id))
+                {
+                    insertSubContent(x, activity_id, language_id, u, article_status_id);
+                }
+                else
+                {
+                    updateSubContent(x, language_id);
+                }
+                old_id.Remove(x.article_id);
+                foreach(int i in old_id)
+                {
+                    ArticleVersion av = db.ArticleVersions.Where(z => z.article_id == i && z.language_id == language_id).FirstOrDefault();
+                    db.ArticleVersions.Remove(av);
+                    ActivityInfo ai = db.ActivityInfoes.Where(z => z.article_id == i && z.main_article == false).FirstOrDefault();
+                    db.ActivityInfoes.Remove(ai);
+                    Article a = db.Articles.Find(i);
+                    db.Articles.Remove(a);
+                }
+                db.SaveChanges();
+            }
+        }
+        public void updateSubContent(subContent data, int language_id)
+        {
+            ArticleVersion av = db.ArticleVersions.Where(x => x.article_id == data.article_id && x.language_id == language_id).FirstOrDefault();
+            av.version_title = data.version_title;
+            av.article_content = data.article_content;
+            db.Entry(av).State = EntityState.Modified;
+        }
+        public void insertSubContent(subContent data, int activity_id, int language_id, Authen.LoginRepo.User u, int article_status_id)
+        {
+            Article a = db.Articles.Add(new Article
+            {
+                account_id = u.account.account_id,
+                article_status_id = article_status_id
+            });
+            db.SaveChanges();
+            db.ActivityInfoes.Add(new ActivityInfo
+            {
+                activity_id = activity_id,
+                article_id = a.article_id,
+                main_article = false
+            });
+            db.ArticleVersions.Add(new ArticleVersion
+            {
+                publish_time = DateTime.Now,
+                version_title = data.version_title,
+                article_content = data.article_content,
+                article_id = a.article_id,
+                language_id = language_id
+            });
+        }
         public class baseDetail
         {
             public string activity_name { get; set; }
@@ -87,6 +225,20 @@ namespace BLL.InternationalCollaboration.AcademicActivity
             public string version_title { get; set; }
             public string article_content { get; set; }
             public int language_id { get; set; }
+        }
+        public class infoDetail : baseDetail
+        {
+            public int activity_id { get; set; }
+        }
+        public class SumDetail
+        {
+            public baseDetail baseDetail { get; set; }
+            public List<subContent> subContent { get; set; }
+        }
+        public class InfoSumDetail
+        {
+            public infoDetail infoDetail { get; set; }
+            public List<subContent> subContent { get; set; }
         }
     }
 }
