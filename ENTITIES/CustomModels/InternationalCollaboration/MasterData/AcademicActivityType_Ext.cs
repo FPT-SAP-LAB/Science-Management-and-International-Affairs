9499e5c1963e3a1365828a45c152a150a5fdd238
Nguyen Ba Son <44664162+N0sBa@users.noreply.github.com>
2021-03-27 02:36:08 +0700

    [edit] - update model + fix bugs AcademicActivityType (#489)
    * [edit] - add master data management to layout
    
    * [edit] - update Right management module
    
    * [edit] - update model for Account management module
    
    * [edit] - update back-end for Academic Collaboration  module
    
    * Update AcademicCollaborationController.cs
    
    * [edit] - update model + academic collaboration module
    
    * [edit] - fix issues found by CodeFactor
    
    * [edit] - update academic collab - long_term - going
    
    * Update AcademicCollaborationRepo.cs
    
    * [edit] - update long-term table
    
    * [edit] - fix issues found by CodeFactor
    
    * [edit] - update model
    
    * Update AcademicCollaborationRepo.cs
    
    * [edit] - update academic collaboration module
    
    * [edit] - delete json report file
    
    * [edit] - update model
    
    * [edit] - update by CodeFactor
    
    * [add] - add more function in Long-term Going case Academic Collaboration
    
    * [edit] - auto edit by pre-commit tool
    
    * [edit] - edit issues found by CodeFactor
    
    * [edit] - update long-term going module
    
    * [edit] - fix issues found by CodeFactor
    
    * [edit] - update long-term going
    
    * [edit] - auto fix by pre-commit tool
    
    * [edit] - update modal add long-term going
    
    * [edit] - auto fix issues by pre-commit tool
    
    * [edit] - update long-term going
    
    * [edit] - auto fix issues by pre-commit
    
    * [edit] - fix issue found by CodeFactor
    
    * [edit] - update model + long-term going
    
    * [edit] - auto fix by pre-commit tool
    
    * [edit] - fix issues found by CodeFactor
    
    * [edit] - update CollabScope & Long-term Going
    
    * [edit] - auto fix by pre-commit tool
    
    * [edit] - update AcademicCollaboration controller
    
    * [edit] - fix issues found by CodeFactor
    
    * [edit] - update long-term going
    
    * [edit] - fix bugs related parameters when execute Query
    
    * [edit] - fix issues found by CodeFactor
    
    * [edit] - update model + update edit long-term going
    
    * [edit] - auto fix by pre-commit
    
    * [edit] - auto fix by pre-commit
    
    * [edit] - update issues by CodeFactor
    
    * [edit] - update issues found by CodeFactor
    
    * [edit] - update model + fix bugs AcademicActivityType
    
    * [edit] - auto fix issues by pre-commit tool
    
    * [edit] - fix issues found by CodeFactor

---
+++ ENTITIES/CustomModels/InternationalCollaboration/MasterData/AcademicActivityType_Ext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITIES.CustomModels.InternationalCollaboration.MasterData
+{
+    public class AcademicActivityType_Ext : AcademicActivityType
+    {
+        public string activity_type_name { get; set; }
+    }
+}
