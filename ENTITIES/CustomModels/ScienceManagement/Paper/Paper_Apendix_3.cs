5be3604483210bcae6870840bf97544f7b56fd6a
hvu0163 <53392914+hvu0163@users.noreply.github.com>
2021-03-27 08:59:02 +0700

    [add] back-end

---
+++ ENTITIES/CustomModels/ScienceManagement/Paper/Paper_Apendix_3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITIES.CustomModels.ScienceManagement.Paper
+{
+    public class Paper_Apendix_3
+    {
+        public string name { get; set; }
+        public string mssv_msnv { get; set; }
+        public string office_abbreviation { get; set; }
+        public int sum_money { get; set; }
+        public string money_string { get; set; }
+    }
+}
