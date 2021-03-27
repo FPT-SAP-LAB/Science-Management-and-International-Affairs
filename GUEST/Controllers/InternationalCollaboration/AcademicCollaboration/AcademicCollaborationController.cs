ce57b506c4d21cdb98685d30bc23c90624d95b4f
Tomuro <tomuro1711@gmail.com>
2021-03-28 00:46:51 +0700

    [fix] format code

--- GUEST/Controllers/InternationalCollaboration/AcademicCollaboration/AcademicCollaborationController.cs
+++ GUEST/Controllers/InternationalCollaboration/AcademicCollaboration/AcademicCollaborationController.cs
@@ -42,105 +42,106 @@ namespace GUEST.Controllers.InternationalCollaboration.AcademicCollaboration
             int language = Int32.Parse(Request.Cookies["language_id"].Value);
             List<ProgramInfo> data = guestRepo.listProgram(count, 2, language);
         }
-            int language = Int32.Parse(Request.Cookies["language_id"].Value);
-            List<ProgramInfo> data = guestRepo.listPartnerProgram(language);
-            return Json(new { success = true, data = data }, JsonRequestBehavior.AllowGet);
+        int language = Int32.Parse(Request.Cookies["language_id"].Value);
+        List<ProgramInfo> data = guestRepo.listPartnerProgram(language);
+            return Json(new { success = true, data = data
+    }, JsonRequestBehavior.AllowGet);
         }
-        [HttpPost]
-        public ActionResult FPT_Program()
-        {
-            int language = Int32.Parse(Request.Cookies["language_id"].Value);
-            List<ProgramInfo> data = guestRepo.listFPTProgram(language);
-            return Json(new { success = true, data = data }, JsonRequestBehavior.AllowGet);
-        }
-        public ActionResult Short_Term()
-        {
-            var pagesTree = new List<PageTree>
+[HttpPost]
+public ActionResult FPT_Program()
+{
+    int language = Int32.Parse(Request.Cookies["language_id"].Value);
+    List<ProgramInfo> data = guestRepo.listFPTProgram(language);
+    return Json(new { success = true, data = data }, JsonRequestBehavior.AllowGet);
+}
+public ActionResult Short_Term()
+{
+    var pagesTree = new List<PageTree>
             {
                 new PageTree("Trao đổi cán bộ giảng viên", "/AcademicCollaboration/Short_Term"),
             };
-            ViewBag.pagesTree = pagesTree;
-            return View();
-        }
+    ViewBag.pagesTree = pagesTree;
+    return View();
+}
 
-        public ActionResult Load_More_List_Short_Term()
-        {
-            try
-            {
-                return Json("", JsonRequestBehavior.AllowGet);
-            }
-            catch (Exception)
-            {
-                return Json("", JsonRequestBehavior.AllowGet);
-            }
-        }
+public ActionResult Load_More_List_Short_Term()
+{
+    try
+    {
+        return Json("", JsonRequestBehavior.AllowGet);
+    }
+    catch (Exception)
+    {
+        return Json("", JsonRequestBehavior.AllowGet);
+    }
+}
 
-        public ActionResult Program_Detail(string id, string type_program)
-        {
-            var pagesTree = new List<PageTree>();
-            switch (type_program)
-            {
-                case "1":
-                    pagesTree = new List<PageTree>
+public ActionResult Program_Detail(string id, string type_program)
+{
+    var pagesTree = new List<PageTree>();
+    switch (type_program)
+    {
+        case "1":
+            pagesTree = new List<PageTree>
             {
                 new PageTree("Đào tạo sau đại học", "/AcademicCollaboration/Long_Term"),
                 new PageTree("Đào tạo của đối tác", "/AcademicCollaboration/Program_Detail"),
                 //new PageTree("Đào tạo của đối tác", "/AcademicCollaboration/Program_Detail?id=" + id),
             };
-                    break;
-                case "2":
-                    pagesTree = new List<PageTree>
+            break;
+        case "2":
+            pagesTree = new List<PageTree>
             {
                 new PageTree("Đào tạo sau đại học", "/AcademicCollaboration/Long_Term"),
                 new PageTree("Đào tạo của FPT", "/AcademicCollaboration/Program_Detail"),
                 //new PageTree("Đào tạo của FPT", "/AcademicCollaboration/Program_Detail?id=" + id),
             };
-                    break;
-                case "3":
-                    pagesTree = new List<PageTree>
+            break;
+        case "3":
+            pagesTree = new List<PageTree>
             {
                 new PageTree("Trao đổi cán bộ giảng viên", "/AcademicCollaboration/Short_Term"),
                 new PageTree("Trao đổi với đối tác", "/AcademicCollaboration/Program_Detail"),
                 //new PageTree("Trao đổi với đối tác", "/AcademicCollaboration/Program_Detail?id=" + id),
             };
-                    break;
-                case "4":
-                    pagesTree = new List<PageTree>
+            break;
+        case "4":
+            pagesTree = new List<PageTree>
             {
                 new PageTree("Trao đổi cán bộ giảng viên", "/AcademicCollaboration/Short_Term"),
                 new PageTree("Trao đổi với FPT", "/AcademicCollaboration/Program_Detail"),
                 //new PageTree("Trao đổi với FPT", "/AcademicCollaboration/Program_Detail?id=" + id),
             };
-                    break;
-            }
-            ViewBag.pagesTree = pagesTree;
-            return View();
-        }
+            break;
+    }
+    ViewBag.pagesTree = pagesTree;
+    return View();
+}
 
-        public ActionResult Procedure_Detail(string id, string type_procedure)
-        {
-            var pagesTree = new List<PageTree>();
-            switch (type_procedure)
-            {
-                case "1":
-                    pagesTree = new List<PageTree>
+public ActionResult Procedure_Detail(string id, string type_procedure)
+{
+    var pagesTree = new List<PageTree>();
+    switch (type_procedure)
+    {
+        case "1":
+            pagesTree = new List<PageTree>
             {
                 new PageTree("Trao đổi cán bộ giảng viên", "/AcademicCollaboration/Short_Term"),
                 new PageTree("Thủ tục với đối tác", "/AcademicCollaboration/Procedure_Detail"),
                 //new PageTree("Đào tạo của đối tác", "/AcademicCollaboration/Procedure_Detail?id=" + id),
             };
-                    break;
-                case "2":
-                    pagesTree = new List<PageTree>
+            break;
+        case "2":
+            pagesTree = new List<PageTree>
             {
                 new PageTree("Trao đổi cán bộ giảng viên", "/AcademicCollaboration/Short_Term"),
                 new PageTree("Thủ tục với FPT", "/AcademicCollaboration/Procedure_Detail"),
                 //new PageTree("Đào tạo của FPT", "/AcademicCollaboration/Procedure_Detail?id=" + id),
             };
-                    break;
-            }
-            ViewBag.pagesTree = pagesTree;
-            return View();
-        }
+            break;
+    }
+    ViewBag.pagesTree = pagesTree;
+    return View();
+}
     }
 }
