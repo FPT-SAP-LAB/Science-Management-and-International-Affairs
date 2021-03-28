b1edbfb281b787a5f7e57771e67514ff3466ec35
nguyenttt11 <55972353+nguyenttt11@users.noreply.github.com>
2021-03-28 20:42:25 +0700

    [Edit] - ConferenceReport (#522)
    * [edit]-sửa lại tiêu đề mấy nút trong màn add Researcher
    
    * [Edit]-List nhân viên
    
    * [add] Report
    
    * [Edit]-ConferencesReport
    
    * [Edit]-report
    
    * Update ConferencesParticipationList.cs (#524)
    
    * Update ConferencesParticipationList.cs (#525)
    
    Co-authored-by: thang1160 <35557579+thang1160@users.noreply.github.com>

--- MANAGER/Controllers/ScienceManagement/Reports/ConferencesParticipationReportController.cs
+++ MANAGER/Controllers/ScienceManagement/Reports/ConferencesParticipationReportController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using BLL.ModelDAL;
+using BLL.ScienceManagement.Report;
+using ENTITIES.CustomModels;
+using ENTITIES.CustomModels.ScienceManagement.Report;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +16,29 @@ namespace MANAGER.Controllers.ScienceManagement.Reports
         public ActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public JsonResult List()
+        {
+            ConferencesParticipationList repo = new ConferencesParticipationList();
+            BaseDatatable datatable = new BaseDatatable(Request);
+            BaseServerSideData<ConferencesParticipationReport> output = repo.ListConferences(datatable);
+            for (int i = 0; i < output.Data.Count; i++)
+            {
+                output.Data[i].RowNumber = datatable.Start + 1 + i;
+                output.Data[i].CreatedDate = output.Data[i].valid_date.ToString("dd/MM/yyyy");
+            }
+            return Json(new { success = true, data = output.Data, draw = Request["draw"], recordsTotal = output.RecordsTotal, recordsFiltered = output.RecordsTotal }, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult GetPeopleByName(String name)
+        {
+            PeopleRepo repo = new PeopleRepo();
+            return Json(repo.GetPeopleName(name, 10), JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult GetOfficeName(String name)
+        {
+            OfficeRepo repo = new OfficeRepo();
+            return Json(repo.GetOfficeName(name, 10), JsonRequestBehavior.AllowGet);
         }
     }
 }
