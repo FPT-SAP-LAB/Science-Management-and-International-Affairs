f1fcfa39e8753348731c61cc91d5970819746a0c
thang1160 <35557579+thang1160@users.noreply.github.com>
2021-03-29 14:00:06 +0700

    [edit] đơn giản hóa PageTree và thêm attribute ajax only (#542)

---
+++ GUEST/Models/AjaxOnlyAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Web.Mvc;
+
+namespace GUEST.Models
+{
+    public class AjaxOnlyAttribute : ActionMethodSelectorAttribute
+    {
+        public override bool IsValidForRequest(ControllerContext controllerContext, MethodInfo methodInfo)
+        {
+            return controllerContext.HttpContext.Request?.Headers["X-Requested-With"] == "XMLHttpRequest";
+        }
+    }
+}
