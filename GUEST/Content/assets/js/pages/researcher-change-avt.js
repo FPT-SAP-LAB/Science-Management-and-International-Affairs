ca6a6b6f83d14b63424f2dc3f06e0fca39e58f6c
Quoc Tuan <39939107+quoctuanck99@users.noreply.github.com>
2021-03-30 21:18:08 +0700

    tuankq

---
+++ GUEST/Content/assets/js/pages/researcher-change-avt.js
@@ -0,0 +1,87 @@
+﻿// Class definition
+toastr.options = {
+    "closeButton": false,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": true,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "30000",
+    "hideDuration": "10000",
+    "timeOut": "50000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+};
+var KTImageInputDemo = function () {
+    // Private functions
+    var initDemos = function () {
+        var avatar5 = new KTImageInput('kt_image_avatar');
+        avatar5.on('cancel', function (imageInput) {
+            swal.fire({
+                title: 'Image successfully changed !',
+                type: 'success',
+                buttonsStyling: false,
+                confirmButtonText: 'Awesome!',
+                confirmButtonClass: 'btn btn-primary font-weight-bold'
+            });
+        });
+        
+        avatar5.on('change', function (imageInput) {
+            toastr.success("Đang thay đổi ảnh hồ sơ....");
+            url = new URL(window.location.href);
+            people_id = url.searchParams.get("id");
+            var fd = new FormData()
+            fd.append('imageInput', imageInput["input"]["files"][0])
+            fd.append('people_id', people_id)
+
+            $.ajax({
+                url: "/Researchers/EditProfilePhoto",
+                type: "POST",
+                data: fd,
+                processData: false,
+                contentType: false,
+                success: function (response) {
+                    if (response.res == 1) {
+                        swal.fire({
+                            title: 'Cập nhật ảnh hồ sơ thành công!',
+                            type: 'success',
+                            buttonsStyling: false,
+                            confirmButtonText: 'OK',
+                            confirmButtonClass: 'btn btn-primary font-weight-bold'
+                        });
+                        window.location.reload()
+                    }
+                    else window.location.reload()
+                },
+                error: function () {
+                    //alert("fail");
+                }
+            });
+        });
+
+        avatar5.on('remove', function (imageInput) {
+            swal.fire({
+                title: 'Image successfully removed !',
+                type: 'error',
+                buttonsStyling: false,
+                confirmButtonText: 'Got it!',
+                confirmButtonClass: 'btn btn-primary font-weight-bold'
+            });
+        });
+    }
+
+    return {
+        // public functions
+        init: function () {
+            initDemos();
+        }
+    };
+}();
+
+KTUtil.ready(function () {
+    KTImageInputDemo.init();
+});
