d3c83d362515d6179be0cd8e0a8b31317ccd74a2
thang1160 <35557579+thang1160@users.noreply.github.com>
2021-03-31 01:34:22 +0700

    [edit] gen lại model (#597)

--- ENTITIES/Model1.Context.cs
+++ ENTITIES/Model1.Context.cs
@@ -115,6 +115,7 @@ namespace ENTITIES
         public virtual DbSet<AcademicDegreeType> AcademicDegreeTypes { get; set; }
         public virtual DbSet<Award> Awards { get; set; }
         public virtual DbSet<CustomProfile> CustomProfiles { get; set; }
+        public virtual DbSet<PeoplePosition> PeoplePositions { get; set; }
         public virtual DbSet<ProfileAcademicDegree> ProfileAcademicDegrees { get; set; }
         public virtual DbSet<WorkingProcess> WorkingProcesses { get; set; }
         public virtual DbSet<AuthorInvention> AuthorInventions { get; set; }
