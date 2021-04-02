3a2f6f8f01f1ae7e4d3fcb7133cac72bbab84464
Ctbom <dungpdhe130254@fpt.edu.vn>
2021-04-02 16:49:55 +0700

    [fix] fix bug from MOU Module

---
+++ ENTITIES/CustomModels/InternationalCollaboration/Collaboration/MemorandumOfUnderstanding/MOU/IntersectPeriodMOUDate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITIES.CustomModels.InternationalCollaboration.Collaboration.MemorandumOfUnderstanding.MOU
+{
+    public class IntersectPeriodMOUDate
+    {
+        public int num_check { get; set; }
+        public DateTime mou_start_date { get; set; }
+        public string mou_start_date_string { get; set; }
+        public string mou_end_date_string { get; set; }
+        public DateTime mou_end_date { get; set; }
+        public int mou_id { get; set; }
+        public string mou_code { get; set; }
+    }
+}
--- MANAGER/Controllers/InternationalCollaboration/Collaboration/MemorandumOfUnderstanding/MOUController.cs
+++ MANAGER/Controllers/InternationalCollaboration/Collaboration/MemorandumOfUnderstanding/MOUController.cs
@@ -223,5 +223,18 @@ namespace MANAGER.Controllers.InternationalCollaboration.Collaboration.Memorandu
                 return new HttpStatusCodeResult(400);
             }
         }
+        public ActionResult checkIntersectPeriodMOUDate(List<PartnerInfo> PartnerInfo,string start_date,string end_date)
+        {
+            try
+            {
+                IntersectPeriodMOUDate obj = mou.checkIntersectPeriodMOUDate(PartnerInfo,start_date,end_date);
+                return Json(obj);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return new HttpStatusCodeResult(400);
+            }
+        }
     }
 }
