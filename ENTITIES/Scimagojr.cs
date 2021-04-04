8d50a1113b9da1675e4303731b2e7fc4ae01cffd
hvu0163 <53392914+hvu0163@users.noreply.github.com>
2021-04-04 19:26:14 +0700

    fix] update model (#736)
    * [add] back-end
    
    * Update InventionRepo.cs
    
    * [add] back-end
    
    * [fix] front-end so trich dan
    
    * [add] back-end khen thuong trich dan
    
    * Update PartialViewController.cs
    
    * [fix] front-end
    
    * [add] back-end add reqest paper
    
    * Update PartialViewController.cs
    
    * [fix] gen laị model
    
    * [fix] sua theo db moi
    
    * Update PartialViewScript.js
    
    * Update PartialViewScript.js
    
    * [add] back-end
    
    * [fix] tự sửa lỗi
    
    * [add] back-end
    
    * [fix] tự động sửa
    
    * [fix] sua theo db moi
    
    * Update PaperRepo.cs
    
    * [add] chỉnh sửa tác giả
    
    * [add] back-end
    
    * Update PaperRepo.cs
    
    * [add] thêm cmt
    
    * [add] back-end
    
    * [fix] tự động sửa
    
    * [fix] front-end
    
    * [add] back-end addrequest citation
    
    * [fix] tự động sửa
    
    * [add] back-end edit citaion requset
    
    * Update PartialViewScript.js
    
    * [add] back-end add invention
    
    * Update InventionController.cs
    
    * [add] back-end edit invention
    
    * Update InventionRepo.cs
    
    * Update JSUpload.js
    
    * [fix] tự động sửa
    
    * [fix] sap xep
    
    * [fix] tu dong sua
    
    * [fix] fix bug
    
    * [add] back-end
    
    * [fix] auto sửa
    
    * [add] back-end
    
    * [add] back-end
    
    * [fix] tự động sửa
    
    * Update WaitDecision.cshtml
    
    * [add] back-end
    
    * [add] back-end
    
    * [fix] front-end
    
    * [add] back-end
    
    * Update ResearchersBiographyRepo.cs
    
    * [add] back-end
    
    * [fix] auto sửa
    
    * [fix] update model
    
    * [add] back-end
    
    * [add] back-end
    
    * [fix] db moi
    
    * Update CitationController.cs
    
    * [fix] theo db moi
    
    * [fix] them dau cach
    
    * [fix] theo db moi
    
    * Update AddAuthor.cs
    
    * [fix] paper va thoe db moi
    
    * [fix] paper
    
    * Update PaperRepo.cs
    
    * [fix] paper
    
    * [fix] paper
    
    * [fix]
    
    * Update PaperRepo.cs
    
    * [fix] db moi
    
    * [fix] build
    
    * [fix] paper
    
    * [fix] tu dong sua
    
    * [fix] paper
    
    * [fix] paper
    
    * [fix] paper
    
    * [fix] invention
    
    * [fix] invention
    
    * [fix] paper
    
    * [fix] invention
    
    * [fix] citation
    
    * Update CitationRepo.cs
    
    * [add] phan quyen
    
    * [add] paper
    
    * Update PaperController.cs
    
    * [fix] update model
    
    * [fix]
    
    Co-authored-by: thang1160 <35557579+thang1160@users.noreply.github.com>

--- ENTITIES/Scimagojr.cs
+++ ENTITIES/Scimagojr.cs
@@ -14,21 +14,21 @@ namespace ENTITIES
     
     public partial class Scimagojr
     {
-        public Nullable<int> Rank { get; set; }
-        public double Sourceid { get; set; }
+        public string Rank { get; set; }
+        public string Sourceid { get; set; }
         public string Title { get; set; }
         public string Type { get; set; }
         public string Issn { get; set; }
-        public Nullable<double> SJR { get; set; }
+        public string SJR { get; set; }
         public string SJR_Best_Quartile { get; set; }
         public string H_index { get; set; }
-        public int Total_Docs_2019 { get; set; }
-        public int Total_Docs_3years { get; set; }
-        public int Total_Refs { get; set; }
-        public int Total_Cites_3years { get; set; }
-        public double Citable_Docs_3years { get; set; }
-        public double Cites_Doc_2years { get; set; }
-        public double Ref_Doc { get; set; }
+        public string Total_Docs_2019 { get; set; }
+        public string Total_Docs_3years { get; set; }
+        public string Total_Refs { get; set; }
+        public string Total_Cites_3years { get; set; }
+        public string Citable_Docs_3years { get; set; }
+        public string Cites_Doc_2years { get; set; }
+        public string Ref_Doc { get; set; }
         public string Country { get; set; }
         public string Region { get; set; }
         public string Publisher { get; set; }
