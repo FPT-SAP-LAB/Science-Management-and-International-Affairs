a94752c56a7ecd82f243dd9c3b214043104badc1
Đăng Dũng <47218583+Ctbom@users.noreply.github.com>
2021-04-06 23:03:40 +0700

    [add] set up Unit Test Project using NUnit (#798)
    * [fix] remove redundant code MOU MOA
    
    * [add] fixing add MOU Function
    
    * Update MOURepo.cs
    
    * [fix] update CustomModel folder
    
    * [fix] import model
    
    * [add] add BackEnd code
    
    * [add] add BackEnd code
    
    * [fix] fix Add and Edit ExMOU code
    
    * [fix] fix Add and Edit ExMOU code
    
    * [add] add and fix function of MOU Module
    
    * [add] add and fix function of MOU Module
    
    * [fix] fixing old query MOU
    
    * Update MOARepo.cs
    
    * Update MOAController.cs
    
    * [fix] fixing old query MOU
    
    * [fix] fixing old query MOU
    
    * Update AcademicActivityRepo.cs
    
    * [add] add function backend MOU + MOA
    
    * [add] add function backend MOU + MOA
    
    * [fix] seperate files to Entities Project
    
    * Update MOABasicInfo.cs
    
    * Update PartnerMOARepo.cs
    
    * [fix] fix bug from CodeFactor
    
    * Update PartnerMOAController.cs
    
    * [fix] changed files to ENTITIES project
    
    * [add] create partial view files and new custom css files
    
    * Update MOURepo.cs
    
    * Update MOUController.cs
    
    * [fix] fixing redundant code detected by CodeFactor
    
    * [fix] fix bug of MOU List module
    
    * [fix] fix bug of MOU List module
    
    * [fix] fix bug of MOU List module
    
    * Update BasicInfoMOURepo.cs
    
    * [fix] fix bug of MOU List module
    
    * Update MOUController.cs
    
    * [add] nuget excel (#422) (#423)
    
    * [fix] fix bug of MOA
    
    * [fix] fix bug of MOA
    
    * [fix] update CRUD of Specialization and Scope when adding new MOU
    
    * [fix] update CRUD of Specialization and Scope when adding new MOU
    
    * Update SpecializationRepo.cs
    
    * [fix] update CRUD of Specialization and Scope when adding new MOU
    
    * [fix] fix bug of MOU module
    
    * [fix] fix bug of MOU module
    
    * [fix] change select function of MOU Code
    
    * [fix] change select function of MOU Code
    
    * [fix] change select function of MOU Code
    
    * [fix] fix bug from MOU Module
    
    * [fix] fix bug from MOU Module
    
    * Update PartnerMOARepo.cs
    
    * [fix] fix bug from MOU Module
    
    * [fix] fix bug from MOU Module
    
    * [fix] fix bug from MOU Module
    
    * Update BasicInfoMOURepo.cs
    
    * [fix] fix bug from MOU Module
    
    * [fix] fix bug from MOU Module
    
    * [fix] fix bug from MOU Module
    
    * [fix] fix bug from MOU Module
    
    * [fix] fix bug from MOU Module
    
    * [fix] fix bug from MOU Module
    
    * [fix] fix bug from MOU Module
    
    * Update MOURepo.cs
    
    * [fix] fix bug from MOU Module
    
    * [fix] fix bug from MOU Module
    
    * [fix] fix bug from MOU Module
    
    * Update MOARepo.cs
    
    * fix] fix bug from MOU Module
    
    * [add] set up Unit Test Project using NUnit
    
    * [add] set up Unit Test Project using NUnit
    
    Co-authored-by: thang1160 <35557579+thang1160@users.noreply.github.com>

---
+++ UnitTest/Lib.cs
@@ -0,0 +1,52 @@
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System;
+using System.Collections;
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class Lib
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [Test]
+        public void ClassicModel()
+        {
+            //Doc: https://docs.nunit.org/articles/nunit/writing-tests/assertions/assertion-models/classic.html
+
+            //For example:
+            Assert.AreEqual(1, 1, "Message error 1");
+        }
+        [Test]
+        public void ConstraintModel()
+        {
+            //Doc: https://docs.nunit.org/articles/nunit/writing-tests/assertions/assertion-models/constraint.html
+
+            //For example:
+            int[] array = new int[] { 1, 2, 3 };
+            //check array has exactly one value = 3
+            Assert.That(array, Has.Exactly(1).EqualTo(3));
+        }
+        [Test]
+        public void MultiAssert()
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(5.2, 5.2, "Message error 1");
+                Assert.AreEqual(3.9, 3.9, "Message error 2");
+            });
+        }
+
+        [TestCase(1, 4, 9)]
+        [TestCase(10, 7, 17)]
+        public void TestPreparedTCs(int a, int b, int c)
+        {
+            Assert.AreEqual(a + b, c, "Message error");
+        }
+    }
+}
