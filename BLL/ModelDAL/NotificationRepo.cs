a2ebabb9147e80f1f56ea21f3f53618b526d1b0c
thang1160 <35557579+thang1160@users.noreply.github.com>
2021-04-07 03:26:20 +0700

    Update NotificationRepo.cs

--- BLL/ModelDAL/NotificationRepo.cs
+++ BLL/ModelDAL/NotificationRepo.cs
@@ -64,16 +64,15 @@ namespace BLL.ModelDAL
                         }).Skip(start).Take(10).ToList();
             return list;
         }
-    }
-
-    public class Notification
-    {
-        public string Icon { get; set; }
-        public string Template { get; set; }
-        public bool IsRead { get; set; }
-        public string URL { get; set; }
-        public DateTime CreatedDate { get; set; }
-        public int AccountID { get; set; }
-        public string StringDate { get; set; }
+        public class Notification
+        {
+            public string Icon { get; set; }
+            public string Template { get; set; }
+            public bool IsRead { get; set; }
+            public string URL { get; set; }
+            public DateTime CreatedDate { get; set; }
+            public int AccountID { get; set; }
+            public string StringDate { get; set; }
+        }
     }
 }
