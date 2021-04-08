5a8c956ca55f866d29b5cc0b8a227dab10b49baa
thang1160 <35557579+thang1160@users.noreply.github.com>
2021-04-08 12:39:46 +0700

    [add] màn cập nhật QS1000

---
+++ BLL/ModelDAL/QsUniversityRepo.cs
@@ -0,0 +1,22 @@
+﻿using ENTITIES;
+using ENTITIES.CustomModels;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+
+namespace BLL.ModelDAL
+{
+    public class QsUniversityRepo
+    {
+        ScienceAndInternationalAffairsEntities db;
+        public BaseServerSideData<QsUniversity> List(BaseDatatable baseDatatable)
+        {
+            db = new ScienceAndInternationalAffairsEntities();
+            var data = db.QsUniversities.OrderBy(baseDatatable.SortColumnName + " " + baseDatatable.SortDirection)
+                .Skip(baseDatatable.Start).Take(baseDatatable.Length).ToList();
+
+            int recordsTotal = db.QsUniversities.Count();
+
+            return new BaseServerSideData<QsUniversity>(data, recordsTotal);
+        }
+    }
+}
---
+++ MANAGER/Controllers/ScienceManagement/ConferenceSponsor/QsUniversityController.cs
@@ -0,0 +1,30 @@
+﻿using BLL.ModelDAL;
+using ENTITIES;
+using ENTITIES.CustomModels;
+using GUEST.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MANAGER.Controllers.ScienceManagement.ConferenceSponsor
+{
+    public class QsUniversityController : Controller
+    {
+        private readonly QsUniversityRepo qsUniversityRepo = new QsUniversityRepo();
+        // GET: QsUniversity
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AjaxOnly]
+        public JsonResult List()
+        {
+            BaseDatatable datatable = new BaseDatatable(Request);
+            BaseServerSideData<QsUniversity> output = qsUniversityRepo.List(datatable);
+            return Json(new { success = true, data = output.Data, draw = Request["draw"], recordsTotal = output.RecordsTotal, recordsFiltered = output.RecordsTotal }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
