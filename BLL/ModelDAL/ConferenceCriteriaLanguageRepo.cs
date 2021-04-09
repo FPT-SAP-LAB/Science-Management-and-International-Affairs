d4286e8239629c5702fb232eba6d262aa2d99e1d
thang1160 <35557579+thang1160@users.noreply.github.com>
2021-04-09 14:14:23 +0700

    [add] thêm màn quản lý điều kiện hỗ trợ hội nghị

--- BLL/ModelDAL/ConferenceCriteriaLanguageRepo.cs
+++ BLL/ModelDAL/ConferenceCriteriaLanguageRepo.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ENTITIES;
+using ENTITIES.CustomModels;
 
 namespace BLL.ModelDAL
 {
     public class ConferenceCriteriaLanguageRepo
     {
         private ScienceAndInternationalAffairsEntities db;
-        public List<string> GetCurrentList(int language_id)
+        public List<ConferenceCriteriaLanguage> GetCurrentList(int language_id)
         {
             db = new ScienceAndInternationalAffairsEntities();
             db.Configuration.LazyLoadingEnabled = false;
@@ -18,8 +19,25 @@ namespace BLL.ModelDAL
                                                join b in db.Criteria on a.policy_id equals b.policy_id
                                                join c in db.ConferenceCriteriaLanguages on b.criteria_id equals c.criteria_id
                                                where a.expired_date == null && c.language_id == language_id
-                                               select c.name).ToList();
+                                               select c).ToList();
             return ConferenceCriteriaLanguages;
+        }
+        public AlertModal<string> Edit(int id, string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                return new AlertModal<string>(false, "Không được bỏ trống");
+            db = new ScienceAndInternationalAffairsEntities();
+            ConferenceCriteriaLanguage criteriaLanguage = db.ConferenceCriteriaLanguages.Find(id);
+            if (criteriaLanguage == null)
+            {
+                return new AlertModal<string>(false, "Không tìm thấy");
+            }
+            else
+            {
+                criteriaLanguage.name = name.Trim();
+                db.SaveChanges();
+                return new AlertModal<string>(name.Trim(), true);
+            }
         }
     }
 }
