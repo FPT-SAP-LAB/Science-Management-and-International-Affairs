eae3077ad86be3a65754ae39592078c8aead5672
tuankq <39939107+quoctuanck99@users.noreply.github.com>
2021-04-11 22:30:56 +0700

    Update bundle.js

--- MANAGER/Content/assets/js/bundle.js
+++ MANAGER/Content/assets/js/bundle.js
@@ -38,12 +38,10 @@ $(".number-comma").each(function () {
     $(this).attr('data-value', $(this).val())
     $(this).text(AddComma($(this).val()))
 })
-$(function () {
-    $(".menu-link").each(function () {
-        if ($(this).attr('href').trim() == window.location.pathname) {
-            $(this).parent().addClass('menu-item-active')
-            $(this).parent().parent().parent().parent().addClass('menu-item-open')
-            $(this).parent().parent().parent().parent().addClass(' menu-item-here')
-        }
-    });
-})
+$(".menu-link").each(function () {
+    if ($(this).attr('href').trim() == window.location.pathname) {
+        $(this).parent().addClass('menu-item-active')
+        $(this).parent().parent().parent().parent().addClass('menu-item-open')
+        $(this).parent().parent().parent().parent().addClass(' menu-item-here')
+    }
+});
