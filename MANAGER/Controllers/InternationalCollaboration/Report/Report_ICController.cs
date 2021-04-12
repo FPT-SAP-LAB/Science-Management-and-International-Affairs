22f2d281681d9de640022e2e199ddc8724cdde40
sonnh <yggdrasil2769@gmail.com>
2021-04-12 15:57:01 +0700

    [add] - add function reload on select dashboard IA

--- MANAGER/Controllers/InternationalCollaboration/Report/Report_ICController.cs
+++ MANAGER/Controllers/InternationalCollaboration/Report/Report_ICController.cs
@@ -19,18 +19,6 @@ namespace MANAGER.Controllers.InternationalCollaboration.Report
         {
             dashboardRepo = new DashboardRepo();
             List<ChartDashboard> dashboards = dashboardRepo.GetDashboard(DateTime.Now.Year);
-            List<int?> year = new List<int?>();
-            List<int?> signed = new List<int?>();
-            List<int?> not_sign_yet = new List<int?>();
-            foreach (var i in dashboards)
-            {
-                year.Add(i.year);
-                signed.Add(i.signed);
-                not_sign_yet.Add(i.not_sign_yet);
-            }
-            ViewBag.year = Json(new { year });
-            ViewBag.signed = Json(new { signed });
-            ViewBag.not_sign_yet = Json(new { not_sign_yet });
             int this_year = DateTime.Today.Year;
             //23 22 21 20 19 18 17 16 15
             List<int> year_select = new List<int>
@@ -44,10 +32,40 @@ namespace MANAGER.Controllers.InternationalCollaboration.Report
                 year_select.Add(this_year - i);
             }
             ViewBag.year_select = year_select;
-            ViewBag.widget_mou = dashboardRepo.WidgetMou(this_year);
-            ViewBag.widget_collab = dashboardRepo.WidgetCollab(this_year);
-            ViewBag.widget_support = dashboardRepo.WidgetSupport(this_year);
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult LoadData(string year_select)
+        {
+            try
+            {
+                int this_year = DateTime.Today.Year;
+                if (year_select != null)
+                {
+                    this_year = Int32.Parse(year_select);
+                }
+                dashboardRepo = new DashboardRepo();
+                List<ChartDashboard> dashboards = dashboardRepo.GetDashboard(this_year);
+                List<int?> year = new List<int?>();
+                List<int?> signed = new List<int?>();
+                List<int?> not_sign_yet = new List<int?>();
+                foreach (var i in dashboards)
+                {
+                    year.Add(i.year);
+                    signed.Add(i.signed);
+                    not_sign_yet.Add(i.not_sign_yet);
+                }
+                string widget_mou = dashboardRepo.WidgetMou(this_year);
+                string widget_collab = dashboardRepo.WidgetCollab(this_year) + "";
+                string widget_support = dashboardRepo.WidgetSupport(this_year) + "";
+                return Json(new { success = true, year, signed, not_sign_yet, 
+                    widget_mou, widget_collab, widget_support});
+            }
+            catch (Exception e)
+            {
+                return Json(new { success = false, error = e.Message });
+            }
         }
 
         public ActionResult LoadTable(int collab_type_id, int year)
