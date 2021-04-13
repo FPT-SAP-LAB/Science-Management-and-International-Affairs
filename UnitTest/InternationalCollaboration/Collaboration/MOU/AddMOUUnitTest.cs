a572ef694f8dabc40ec97a85e95090e1f7e227dc
Ctbom <dungpdhe130254@fpt.edu.vn>
2021-04-13 15:13:13 +0700

    Update AddMOUUnitTest.cs

--- UnitTest/InternationalCollaboration/Collaboration/MOU/AddMOUUnitTest.cs
+++ UnitTest/InternationalCollaboration/Collaboration/MOU/AddMOUUnitTest.cs
@@ -29,7 +29,7 @@ namespace UnitTest.InternationalCollaboration.Collaboration
                 BasicInfo = new BasicInfo
                 {
                     mou_code = "2020/99",
-                    mou_end_date = "TestAddMOU1",
+                    mou_end_date = "20/5/2025",
                     mou_note = "TestAddMOU1",
                     office_id = 1,
                     mou_status_id = 2,
@@ -45,7 +45,62 @@ namespace UnitTest.InternationalCollaboration.Collaboration
                     phone_add = "0123456789",
                     email_add = "TestAddMOU1",
                     coop_scope_add = new List<int>() { 3, 5 },
-                    specialization_add = new List<int>() { 3 }
+                    specialization_add = new List<int>() { 3 },
+                    sign_date_mou_add = "20/5/2020"
+                }
+            );
+            mou.addMOU(input, user);
+            Assert.Pass();
+        }
+        [TestCase]
+        public void TestAddMOU2()
+        {
+            //Prepare data:
+            MOURepo mou = new MOURepo();
+            BLL.Authen.LoginRepo.User user = new BLL.Authen.LoginRepo.User
+            {
+                account = new ENTITIES.Account
+                {
+                    account_id = 16
+                }
+            };
+            //Add MOU with 1 old partner + 1 new partner.
+            MOUAdd input = new MOUAdd
+            {
+                BasicInfo = new BasicInfo
+                {
+                    mou_code = "2020/999",
+                    mou_end_date = "20/5/2025",
+                    mou_note = "TestAddMOU2",
+                    office_id = 2,
+                    mou_status_id = 2,
+                    reason = "TestAddMOU2"
+                },
+                PartnerInfo = new List<PartnerInfo>()
+            };
+            input.PartnerInfo.AddRange(
+                new List<PartnerInfo> 
+                {
+                    new PartnerInfo
+                    {
+                        partner_id = 1,
+                        represent_add = "TestAddMOU2",
+                        phone_add = "0123456789",
+                        email_add = "TestAddMOU1",
+                        coop_scope_add = new List<int>() { 3, 5 },
+                        specialization_add = new List<int>() { 3 },
+                        sign_date_mou_add = "20/5/2020"
+                    },
+                    new PartnerInfo
+                    {
+                        partner_id = 1,
+                        represent_add = "TestAddMOU2",
+                        phone_add = "0123456789",
+                        email_add = "TestAddMOU1",
+                        coop_scope_add = new List<int>() { 3, 5 },
+                        specialization_add = new List<int>() { 3 },
+                        sign_date_mou_add = "20/5/2020"
+                    }
                 }
             );
             mou.addMOU(input, user);
