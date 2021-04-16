750f3372fa327140c2cd434db4987f35aa94cc37
Quoc Tuan <39939107+quoctuanck99@users.noreply.github.com>
2021-04-17 03:23:51 +0700

    [add, fix] report award by authors, fix login GUEST javascript problem (#1061)
    * tuankq
    
    * tuankq
    
    * [fix] UI - Conference Sponsor Manager
    
    * a
    
    * Update bundle.js
    
    * tuankq
    
    * [fix] solve issues
    
    * tuankq
    
    * tuankq
    
    * tuankq
    
    * tuankq
    
    * tuankq
    
    * [add] notification number
    
    * update edmx after change database
    
    * tuankq
    
    * [fix] conflict code after change database and ENTITIES (in paper module)
    
    * fix
    
    * [fix] Icon in navbar admin
    
    * [fix] researcher awards
    
    * [fix] solve issues #466 #465, etc
    
    * tuankq
    
    * tuankq
    
    * tuankq
    
    * [fix] wrong query paper pending (MANAGER)
    
    * [add, fix] report award by authors
    
    * Update RewardsReportRepo.cs

---
+++ ENTITIES/CustomModels/ScienceManagement/Report/ReportByAuthorAward.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENTITIES.CustomModels.ScienceManagement.Report
+{
+    public class ReportByAuthorAward
+    {
+        public int rowNum { get; set; }
+        public string name { get; set; }
+        public string title { get; set; }
+        public string office { get; set; }
+        public string paperAward { get; set; }
+        public string conferenceAward { get; set; }
+        public string CitationAward { get; set; }
+    }
+}
