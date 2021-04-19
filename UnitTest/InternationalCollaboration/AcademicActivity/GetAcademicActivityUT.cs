03b98edecb61077d1c3a1b3c26060d943d6c714d
Tomuro <tomuro1711@gmail.com>
2021-04-19 16:49:11 +0700

    [fix] fix change language activity detail guest

---
+++ UnitTest/InternationalCollaboration/AcademicActivity/GetAcademicActivityUT.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using BLL.InternationalCollaboration.AcademicActivity;
+using static BLL.InternationalCollaboration.AcademicActivity.AcademicActivityGuestRepo;
+
+namespace UnitTest.InternationalCollaboration.AcademicActivity
+{
+    [TestFixture]
+    class GetAcademicActivityUT
+    {
+        [TestCase]
+        public void GetAcademicActivityUT_1()
+        {
+            AcademicActivityGuestRepo guestRepo = new AcademicActivityGuestRepo();
+            List<int> type = new List<int> { 1, 2, 3, 4 };
+            List<baseAA> alist = guestRepo.getBaseAA(0, type, 1, null);
+            Assert.Pass();
+        }
+        [TestCase]
+        public void GetAcademicActivityUT_2()
+        {
+
+        }
+    }
+}
