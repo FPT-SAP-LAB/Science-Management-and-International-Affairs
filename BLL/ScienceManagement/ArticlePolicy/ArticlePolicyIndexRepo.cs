dfa2a0602002c6710878bbb30ef1e7320a6f2904
thang1160 <35557579+thang1160@users.noreply.github.com>
2021-04-20 22:13:20 +0700

    [add] màn quản lý thông tin chính sách (#1113)

--- BLL/ScienceManagement/ArticlePolicy/ArticlePolicyIndexRepo.cs
+++ BLL/ScienceManagement/ArticlePolicy/ArticlePolicyIndexRepo.cs
@@ -1,30 +1,32 @@
-﻿using System;
+﻿using ENTITIES;
+using ENTITIES.CustomModels.ScienceManagement.ArticlePolicy;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ENTITIES;
-using ENTITIES.CustomModels.ScienceManagement.ArticlePolicy;
 
 namespace BLL.ScienceManagement.ArticlePolicy
 {
     public class ArticlePolicyIndexRepo
     {
         ScienceAndInternationalAffairsEntities db;
-        public List<ArticlePolicyItem> List(int language_id)
+        public List<ArticlePolicyIndex> List(int language_id)
         {
             db = new ScienceAndInternationalAffairsEntities();
-            var temp = (from a in db.Policies
-                        join b in db.Policy_type on a.policy_type_id equals b.policy_type_id
-                        join c in db.Articles on a.article_id equals c.article_id
-                        where a.expired_date == null && c.article_status_id == 2
-                        select new ArticlePolicyItem
+            var temp = (from c in db.Articles
+                        where c.article_status_id == 2 && c.PolicyTypes.Count > 0
+                        select new ArticlePolicyIndex
                         {
                             ArticleID = c.article_id,
-                            Title = db.ArticleVersions.Where(x => x.language_id == language_id)
+                            Title = c.ArticleVersions.Where(x => x.language_id == language_id)
                             .OrderByDescending(x => x.publish_time).FirstOrDefault().version_title,
-                            TypeName = b.policy_type_name
+                            CreatedBy = c.Account.full_name,
+                            Types = (from b in db.PolicyTypeLanguages
+                                     join d in c.PolicyTypes on b.policy_type_id equals d.policy_type_id
+                                     where b.language_id == language_id
+                                     select b.policy_type_name).ToList(),
+                            LastEdited = c.ArticleVersions.Where(x => x.language_id == language_id)
+                            .OrderByDescending(x => x.publish_time).FirstOrDefault().publish_time
                         }).ToList();
+            temp.ForEach(x => x.TypeName = string.Join(", ", x.Types));
             return temp;
         }
     }
