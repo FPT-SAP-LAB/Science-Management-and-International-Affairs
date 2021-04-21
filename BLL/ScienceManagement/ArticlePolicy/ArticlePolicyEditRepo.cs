dc917fc1f748423f1f39d06fdcf6b036fc9b962c
thang1160 <35557579+thang1160@users.noreply.github.com>
2021-04-21 21:51:51 +0700

    [add] màn chỉnh sửa thông tin chính sách

---
+++ BLL/ScienceManagement/ArticlePolicy/ArticlePolicyEditRepo.cs
@@ -0,0 +1,28 @@
+﻿using ENTITIES;
+using ENTITIES.CustomModels.ScienceManagement.ArticlePolicy;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BLL.ScienceManagement.ArticlePolicy
+{
+    public class ArticlePolicyEditRepo
+    {
+        private ScienceAndInternationalAffairsEntities db;
+        public ArticlePolicyEdit Detail(int article_id)
+        {
+            db = new ScienceAndInternationalAffairsEntities();
+            List<ArticleVersion> Versions = (from a in db.Languages
+                                             orderby a.language_id
+                                             select (from c in db.Articles
+                                                     join b in db.ArticleVersions on c.article_id equals b.article_id
+                                                     where c.article_status_id == 2 && c.article_id == article_id
+                                                     && b.language_id == a.language_id
+                                                     orderby b.publish_time descending
+                                                     select b).FirstOrDefault()).ToList();
+            List<PolicyTypeLanguage> TypeLanguages = (from a in db.PolicyTypeLanguages
+                                                      join b in db.PolicyTypes on a.policy_type_id equals b.policy_type_id
+                                                      select a).ToList();
+            return new ArticlePolicyEdit(Versions, TypeLanguages);
+        }
+    }
+}
--- MANAGER/Controllers/ScienceManagement/Policy/ArticlePolicyController.cs
+++ MANAGER/Controllers/ScienceManagement/Policy/ArticlePolicyController.cs
@@ -7,6 +7,7 @@ using System.Web;
 using System.Web.Mvc;
 using ENTITIES;
 using MANAGER.Models;
+using Newtonsoft.Json;
 
 namespace MANAGER.Controllers.ScienceManagement.ArticlePolicy
 {
@@ -15,11 +16,12 @@ namespace MANAGER.Controllers.ScienceManagement.ArticlePolicy
         private readonly ArticlePolicyIndexRepo IndexRepo = new ArticlePolicyIndexRepo();
         private readonly ArticlePolicyAddRepo AddRepo = new ArticlePolicyAddRepo();
         private readonly ArticlePolicyDetailRepo DetailRepo = new ArticlePolicyDetailRepo();
+        private readonly ArticlePolicyEditRepo EditRepo = new ArticlePolicyEditRepo();
         public ActionResult Index()
         {
             int.TryParse(Request["language"], out int language_id);
             language_id = language_id == 0 ? 1 : language_id;
-            ViewBag.items = IndexRepo.ListManager(language_id);
+            ViewBag.items = IndexRepo.List(language_id);
             return View();
         }
         public ActionResult Add()
@@ -42,6 +44,30 @@ namespace MANAGER.Controllers.ScienceManagement.ArticlePolicy
         {
             ViewBag.detail = DetailRepo.Detail(id, 1);
             return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            PolicyTypeLanguageRepo policyTypeLanguageRepo = new PolicyTypeLanguageRepo();
+            List<Language> languages = LanguageRepo.GetLanguages();
+            var types = policyTypeLanguageRepo.PolicyTypeLanguages(languages[0].language_id)
+                .Select(x => new
+                {
+                    x.policy_type_language_id,
+                    x.policy_type_name,
+                    value = x.policy_type_name
+                }).ToList();
+            ViewBag.types = JsonConvert.SerializeObject(types);
+            ViewBag.languages = languages;
+            ViewBag.detail = EditRepo.Detail(id);
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public JsonResult Edit(List<ArticleVersion> versions, List<int> types)
+        {
+            return Json(AddRepo.Add(versions, types, CurrentAccount.AccountID(Session)));
         }
     }
 }
