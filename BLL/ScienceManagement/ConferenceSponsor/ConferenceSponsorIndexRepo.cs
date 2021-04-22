bbedbd49d8abb30721264b378e0240415603849f
thang1160 <35557579+thang1160@users.noreply.github.com>
2021-04-23 02:14:45 +0700

    [add] generic class (#1152)

--- BLL/ScienceManagement/ConferenceSponsor/ConferenceSponsorIndexRepo.cs
+++ BLL/ScienceManagement/ConferenceSponsor/ConferenceSponsorIndexRepo.cs
@@ -1,29 +1,28 @@
 ﻿using ENTITIES;
 using ENTITIES.CustomModels;
 using ENTITIES.CustomModels.ScienceManagement.Conference;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Dynamic.Core;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BLL.ScienceManagement.ConferenceSponsor
 {
     public class ConferenceSponsorIndexRepo
     {
         readonly ScienceAndInternationalAffairsEntities db = new ScienceAndInternationalAffairsEntities();
-        public BaseServerSideData<ConferenceIndex> GetIndexPage(BaseDatatable baseDatatable, string search_paper, string search_conference, int search_status, int account_id = 0, int language_id = 1)
+        public BaseServerSideData<ConferenceIndex> GetIndexPage(BaseDatatable baseDatatable, ConferenceSearch search, int account_id = 0, int language_id = 1)
         {
+            search.SearchConference = search.SearchConference ?? "";
+            search.SearchPaper = search.SearchPaper ?? "";
             var data = (from a in db.Papers
                         join b in db.RequestConferences on a.paper_id equals b.paper_id
                         join c in db.Conferences on b.conference_id equals c.conference_id
                         join d in db.BaseRequests on b.request_id equals d.request_id
                         join e in db.ConferenceStatusLanguages on b.status_id equals e.status_id
                         join f in db.Accounts on d.account_id equals f.account_id
-                        where e.language_id == language_id && (b.status_id != 5 || account_id != 0) && (d.account_id == account_id || account_id == 0)
-                        && a.name.Contains(search_paper) && c.conference_name.Contains(search_conference) && (b.status_id == search_status || search_status == 0)
+                        where e.language_id == language_id && (b.status_id != 5 || account_id != 0)
+                        && (d.account_id == account_id || account_id == 0)
+                        && a.name.Contains(search.SearchPaper) && c.conference_name.Contains(search.SearchConference)
+                        && (b.status_id == search.SearchStatus || search.SearchStatus == 0)
                         select new ConferenceIndex
                         {
                             RequestID = d.request_id,
@@ -43,7 +42,8 @@ namespace BLL.ScienceManagement.ConferenceSponsor
                                 join c in db.Conferences on b.conference_id equals c.conference_id
                                 join d in db.BaseRequests on b.request_id equals d.request_id
                                 where (b.status_id != 5 || account_id != 0) && (d.account_id == account_id || account_id == 0)
-                                && a.name.Contains(search_paper) && c.conference_name.Contains(search_conference) && (b.status_id == search_status || search_status == 0)
+                                && a.name.Contains(search.SearchPaper) && c.conference_name.Contains(search.SearchConference)
+                                && (b.status_id == search.SearchStatus || search.SearchStatus == 0)
                                 select b).Count();
 
             for (int i = 0; i < data.Count; i++)
@@ -55,6 +55,8 @@ namespace BLL.ScienceManagement.ConferenceSponsor
         }
         public BaseServerSideData<ConferenceIndex> GetHistoryPage(BaseDatatable baseDatatable, string search_paper, string search_conference)
         {
+            search_paper = search_paper ?? "";
+            search_conference = search_conference ?? "";
             var data = (from a in db.Papers
                         join b in db.RequestConferences on a.paper_id equals b.paper_id
                         join c in db.Conferences on b.conference_id equals c.conference_id
