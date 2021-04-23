ef95f15f5477344c2d6f1652a2af17b4f8a57ae8
Nguyen Hoang Son <53392660+sonnhhe130366@users.noreply.github.com>
2021-04-23 17:02:30 +0700

    [fix] - fix css long short term guest, partner guest (#1157)

--- BLL/InternationalCollaboration/Collaboration/PartnerRepo/PartnerRepo.cs
+++ BLL/InternationalCollaboration/Collaboration/PartnerRepo/PartnerRepo.cs
@@ -257,9 +257,17 @@ namespace BLL.InternationalCollaboration.Collaboration.PartnerRepo
 
                 ArticleVersion articleVersion = db.ArticleVersions.Where(x => x.article_id == partner.article_id &&
                  x.language_id == language_id).FirstOrDefault();
-                if (articleVersion != null)
+                if (articleVersion != null && !String.IsNullOrWhiteSpace(articleVersion.article_content))
                 {
                     partnerArticle.partner_content = articleVersion.article_content;
+                }
+                else if (language_id == 1)
+                {
+                    partnerArticle.partner_content = "Đối tác này chưa có nội dung mô tả";
+                }
+                else if (language_id == 2)
+                {
+                    partnerArticle.partner_content = "Partner has no description content";
                 }
                 return partnerArticle;
             }
