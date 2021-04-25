d383957f838a76f5482295fbc97e2b5862952d2d
tuankq <39939107+quoctuanck99@users.noreply.github.com>
2021-04-25 17:17:20 +0700

    fixbug/report

---
+++ UnitTest/ScienceManagement/Researchers/ResearcherBiographyTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ENTITIES.CustomModels.ScienceManagement;
+using BLL.ScienceManagement.Researcher;
+using BLL.ScienceManagement.ResearcherListRepo;
+using ENTITIES.CustomModels.Datatable;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.ScienceManagement.Researchers
+{
+
+    [TestFixture]
+    class ResearcherBiographyTest
+    {
+        private ResearchersBiographyRepo _researcherListRepo;
+        [SetUp]
+        public void Setup()
+        {
+            _researcherListRepo = new ResearchersBiographyRepo();
+        }
+        [Test]
+        [DataRow(int.MaxValue)]
+        [DataRow(int.MinValue)]
+        public void GetListResearchersTest(int resId)
+        {
+            BaseDatatable bd = new BaseDatatable();
+            var resList = _researcherListRepo.GetAwards(resId);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(0, resList.Count);
+        }
+        
+    }
+}
