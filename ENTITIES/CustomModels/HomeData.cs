5363ed8321e3f8f45f98c7c1e8d953314ac36e81
thang1160 <35557579+thang1160@users.noreply.github.com>
2021-05-02 01:52:24 +0700

    [add] thêm màn cập nhật ảnh nền và banner (#1236)
    * [add] thêm màn cập nhật ảnh nền và banner
    
    * [add] thêm màn cập nhật ảnh bìa
    
    * [add] cập nhật ảnh bìa

---
+++ ENTITIES/CustomModels/HomeData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace ENTITIES.CustomModels
+{
+    public class HomeData
+    {
+        public int Partner { get; set; }
+        public List<string> Images { get; set; }
+
+        public HomeData()
+        {
+        }
+
+        public HomeData(int partner, List<string> images)
+        {
+            Partner = partner;
+            Images = images;
+        }
+    }
+}
