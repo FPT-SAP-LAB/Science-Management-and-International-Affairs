01acbb60bed5ff2af67c7372294f5e778ab18c10
Đăng Dũng <47218583+Ctbom@users.noreply.github.com>
2021-05-05 02:38:42 +0700

    Fixbug/unit test (#1273)
    * Update App.config
    
    * [add] add UT files
    
    * [add] add UT files
    
    * [add] add UT files
    
    * [add] add UT files

--- UnitTest/Demo.cs
+++ UnitTest/Demo.cs
@@ -1,45 +1,45 @@
-﻿using BLL.InternationalCollaboration.Collaboration.MemorandumOfUnderstanding;
-using ENTITIES.CustomModels.Datatable;
-using ENTITIES.CustomModels.InternationalCollaboration.Collaboration.MemorandumOfUnderstanding.MOU;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-using NUnit.Framework;
-using System;
-using System.Collections.Generic;
+﻿//using BLL.InternationalCollaboration.Collaboration.MemorandumOfUnderstanding;
+//using ENTITIES.CustomModels.Datatable;
+//using ENTITIES.CustomModels.InternationalCollaboration.Collaboration.MemorandumOfUnderstanding.MOU;
+////using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using NUnit.Framework;
+//using System;
+//using System.Collections.Generic;
 
-namespace UnitTest
-{
-    [TestFixture]
-    public class Demo
-    {
-        //[TestCase]
-        public void Demo1()
-        {
-            //Check list count
-            MOURepo mou = new MOURepo();
-            BaseDatatable baseDatatable = new BaseDatatable
-            {
-                SortColumnName = "mou_partner_id",
-                SortDirection = "desc",
-                Start = 0,
-                Length = 10
-            };
-            List<ListMOU> listTest = mou.listAllMOU(baseDatatable, "", "", "").Data;
-            Assert.AreEqual(10, listTest.Count);
-        }
-        [TestCase]
-        public void Demo2()
-        {
-            //Assert.AreEqual(1, 1);
-            //Assert.Inconclusive();
-        }
-        [TestCase]
-        public void Demo3()
-        {
-            Assert.Multiple(() =>
-            {
-                Demo2();
-                Assert.AreEqual(4, 4);
-            });
-        }
-    }
-}
+//namespace UnitTest
+//{
+//    [TestFixture]
+//    public class Demo
+//    {
+//        //[TestCase]
+//        public void Demo1()
+//        {
+//            //Check list count
+//            MOURepo mou = new MOURepo();
+//            BaseDatatable baseDatatable = new BaseDatatable
+//            {
+//                SortColumnName = "mou_partner_id",
+//                SortDirection = "desc",
+//                Start = 0,
+//                Length = 10
+//            };
+//            List<ListMOU> listTest = mou.listAllMOU(baseDatatable, "", "", "").Data;
+//            Assert.AreEqual(10, listTest.Count);
+//        }
+//        [TestCase]
+//        public void Demo2()
+//        {
+//            //Assert.AreEqual(1, 1);
+//            //Assert.Inconclusive();
+//        }
+//        [TestCase]
+//        public void Demo3()
+//        {
+//            Assert.Multiple(() =>
+//            {
+//                Demo2();
+//                Assert.AreEqual(4, 4);
+//            });
+//        }
+//    }
+//}
---
+++ UnitTest/InternationalCollaboration/AcademicCollaborationRepository/TestAcademicCollaborations.cs
@@ -0,0 +1,261 @@
+﻿using BLL.InternationalCollaboration.AcademicActivity;
+using BLL.InternationalCollaboration.AcademicCollaborationRepository;
+using ENTITIES.CustomModels.Datatable;
+using ENTITIES.CustomModels.InternationalCollaboration.AcademicCollaborationEntities;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static BLL.InternationalCollaboration.AcademicActivity.AcademicActivityGuestRepo;
+
+namespace UnitTest.InternationalCollaboration.AcademicCollaborationRepository
+{
+    [TestFixture]
+    class TestAcademicCollaborations
+    {
+        [TestCase]
+        public void TestMethod1()
+        {
+            //Arrange
+            int direction = 1;
+            int collab_type_id = 1;
+            string country_name = "";
+            int year = 2020;
+            string partner_name = "Daekin University";
+            string office_name = "";
+            ObjectSearching_AcademicCollaboration obj_searching = new ObjectSearching_AcademicCollaboration
+            {
+                country_name = country_name,
+                office_name = office_name,
+                partner_name = partner_name,
+                year = year
+            };
+            BaseDatatable baseDatatable = new BaseDatatable
+            {
+                Length = 5,
+                SortColumnName = "collab_id",
+                SortDirection = "asc",
+                Start = 0
+            };
+            //Act
+            new AcademicCollaborationRepo().AcademicCollaborations(direction, collab_type_id, obj_searching, baseDatatable);
+            //Assert
+            Assert.Pass();
+        }
+        [TestCase]
+        public void TestMethod2()
+        {
+            //Arrange
+            int direction = 1;
+            int collab_type_id = 2;
+            string country_name = "";
+            int year = 2020;
+            string partner_name = "Daekin University";
+            string office_name = "";
+            ObjectSearching_AcademicCollaboration obj_searching = new ObjectSearching_AcademicCollaboration
+            {
+                country_name = country_name,
+                office_name = office_name,
+                partner_name = partner_name,
+                year = year
+            };
+            BaseDatatable baseDatatable = new BaseDatatable
+            {
+                Length = 5,
+                SortColumnName = "collab_id",
+                SortDirection = "asc",
+                Start = 0
+            };
+            //Act
+            new AcademicCollaborationRepo().AcademicCollaborations(direction, collab_type_id, obj_searching, baseDatatable);
+            //Assert
+            Assert.Pass();
+        }
+        [TestCase]
+        public void TestMethod3()
+        {
+            //Arrange
+            int direction = 1;
+            int collab_type_id = 1;
+            string country_name = "";
+            int year = 2020;
+            string partner_name = "Daekin University";
+            string office_name = "";
+            ObjectSearching_AcademicCollaboration obj_searching = new ObjectSearching_AcademicCollaboration
+            {
+                country_name = country_name,
+                office_name = office_name,
+                partner_name = partner_name,
+                year = year
+            };
+            BaseDatatable baseDatatable = new BaseDatatable
+            {
+                Length = 5,
+                SortColumnName = "collab_id",
+                SortDirection = "asc",
+                Start = 0
+            };
+            //Act
+            new AcademicCollaborationRepo().AcademicCollaborations(direction, collab_type_id, obj_searching, baseDatatable);
+            //Assert
+            Assert.Pass();
+        }
+        [TestCase]
+        public void TestMethod4()
+        {
+            //Arrange
+            int direction = 1;
+            int collab_type_id = 1;
+            string country_name = "";
+            int year = 2020;
+            string partner_name = "Daekin University";
+            string office_name = "";
+
+            ObjectSearching_AcademicCollaboration obj_searching = new ObjectSearching_AcademicCollaboration
+            {
+                country_name = country_name,
+                office_name = office_name,
+                partner_name = partner_name,
+                year = year
+            };
+            BaseDatatable baseDatatable = new BaseDatatable
+            {
+                Length = 5,
+                SortColumnName = "collab_id",
+                SortDirection = "asc",
+                Start = 0
+            };
+            //Act
+            new AcademicCollaborationRepo().AcademicCollaborations(direction, collab_type_id, obj_searching, baseDatatable);
+
+            //Assert
+            Assert.Pass();
+        }
+        [TestCase]
+        public void TestMethod5()
+        {
+            //Arrange
+            int direction = 1;
+            int collab_type_id = 1;
+            string country_name = "";
+            int year = 2020;
+            string partner_name = "Daekin University";
+            string office_name = "";
+
+            ObjectSearching_AcademicCollaboration obj_searching = new ObjectSearching_AcademicCollaboration
+            {
+                country_name = country_name,
+                office_name = office_name,
+                partner_name = partner_name,
+                year = year
+            };
+            BaseDatatable baseDatatable = new BaseDatatable
+            {
+                Length = 5,
+                SortColumnName = "collab_id",
+                SortDirection = "asc",
+                Start = 0
+            };
+            //Act
+            new AcademicCollaborationRepo().AcademicCollaborations(direction, collab_type_id, obj_searching, baseDatatable);
+
+            //Assert
+            Assert.Pass();
+        }
+        [TestCase]
+        public void TestMethod6()
+        {
+            //Arrange
+            int direction = 1;
+            int collab_type_id = 1;
+            string country_name = "";
+            int year = 2020;
+            string partner_name = "Daekin University";
+            string office_name = "";
+
+            ObjectSearching_AcademicCollaboration obj_searching = new ObjectSearching_AcademicCollaboration
+            {
+                country_name = country_name,
+                office_name = office_name,
+                partner_name = partner_name,
+                year = year
+            };
+            BaseDatatable baseDatatable = new BaseDatatable
+            {
+                Length = 5,
+                SortColumnName = "collab_id",
+                SortDirection = "asc",
+                Start = 0
+            };
+            //Act
+            new AcademicCollaborationRepo().AcademicCollaborations(direction, collab_type_id, obj_searching, baseDatatable);
+
+            //Assert
+            Assert.Pass();
+        }
+        [TestCase]
+        public void TestMethod7()
+        {
+            //Arrange
+            int direction = 1;
+            int collab_type_id = 1;
+            string country_name = "";
+            int year = 2020;
+            string partner_name = "Daekin University";
+            string office_name = "";
+
+            ObjectSearching_AcademicCollaboration obj_searching = new ObjectSearching_AcademicCollaboration
+            {
+                country_name = country_name,
+                office_name = office_name,
+                partner_name = partner_name,
+                year = year
+            };
+            BaseDatatable baseDatatable = new BaseDatatable
+            {
+                Length = 5,
+                SortColumnName = "collab_id",
+                SortDirection = "asc",
+                Start = 0
+            };
+            //Act
+            new AcademicCollaborationRepo().AcademicCollaborations(direction, collab_type_id, obj_searching, baseDatatable);
+
+            //Assert
+            Assert.Pass();
+        }
+        [TestCase]
+        public void TestMethod8()
+        {
+            //Arrange
+            int direction = 1;
+            int collab_type_id = 1;
+            string country_name = "";
+            int year = 2020;
+            string partner_name = "Daekin University";
+            string office_name = "";
+
+            ObjectSearching_AcademicCollaboration obj_searching = new ObjectSearching_AcademicCollaboration
+            {
+                country_name = country_name,
+                office_name = office_name,
+                partner_name = partner_name,
+                year = year
+            };
+            BaseDatatable baseDatatable = new BaseDatatable
+            {
+                Length = 5,
+                SortColumnName = "collab_id",
+                SortDirection = "asc",
+                Start = 0
+            };
+            //Act
+            new AcademicCollaborationRepo().AcademicCollaborations(direction, collab_type_id, obj_searching, baseDatatable);
+
+            //Assert
+            Assert.Pass();
+        }
+    }
+}
