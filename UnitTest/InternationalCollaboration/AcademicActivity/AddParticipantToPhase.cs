23e775df2f7c842c84499ca0b57463da17eecc3e
duongpl <51127228+duongpl@users.noreply.github.com>
2021-05-05 17:51:03 +0700

    [add] - ut for academic activity (#1287)
    * Update DeleteAcademicActivityUT.cs
    
    * [add] - ut for  academic activity
    
    * update
    
    * update

---
+++ UnitTest/InternationalCollaboration/AcademicActivity/AddParticipantToPhase.cs
@@ -0,0 +1,90 @@
+﻿using BLL.InternationalCollaboration.AcademicActivity;
+using ENTITIES;
+using ENTITIES.CustomModels;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.InternationalCollaboration.AcademicActivity
+{
+    [TestFixture]
+    public class AddParticipantToPhase
+    {
+        ScienceAndInternationalAffairsEntities db = new ScienceAndInternationalAffairsEntities();
+        [TestCase]
+        public void AddParticipantToPhaseUT_1()
+        {
+            new AddAcademicActivityPhaseUT().AddAcademicActivityPhaseUT_1();
+            AcademicActivityPhase academicActivityPhase = db.AcademicActivityPhases.Last();
+            CheckInRepo repo = new CheckInRepo();
+            CheckInRepo.infoParticipant obj = new CheckInRepo.infoParticipant
+            {
+                name = "duong",
+                participant_role_id = 101,
+                email = "aa@gmail.com",
+                participant_number = "12328181",
+                office_id = 1
+            };
+            bool res = repo.addParticipant(obj);
+            if (res)
+                Assert.Pass();
+        }
+        [TestCase]
+        public void AddParticipantToPhaseUT_2()
+        {
+            new AddAcademicActivityPhaseUT().AddAcademicActivityPhaseUT_1();
+            AcademicActivityPhase academicActivityPhase = db.AcademicActivityPhases.Last();
+            CheckInRepo repo = new CheckInRepo();
+            CheckInRepo.infoParticipant obj = new CheckInRepo.infoParticipant
+            {
+                name = "!@#!@",
+                participant_role_id = 102,
+                email = "duongpl@gmail.com",
+                participant_number = "",
+                office_id = 0
+            };
+            bool res = repo.addParticipant(obj);
+            if (!res)
+                Assert.Pass();
+        }
+        [TestCase]
+        public void AddParticipantToPhaseUT_3()
+        {
+            new AddAcademicActivityPhaseUT().AddAcademicActivityPhaseUT_1();
+            AcademicActivityPhase academicActivityPhase = db.AcademicActivityPhases.Last();
+            CheckInRepo repo = new CheckInRepo();
+            CheckInRepo.infoParticipant obj = new CheckInRepo.infoParticipant
+            {
+                name = "",
+                participant_role_id = 102,
+                email = "",
+                participant_number = "0128dasda1",
+                office_id = 0
+            };
+            bool res = repo.addParticipant(obj);
+            if (!res)
+                Assert.Pass();
+        }
+        [TestCase]
+        public void AddParticipantToPhaseUT_4()
+        {
+            new AddAcademicActivityPhaseUT().AddAcademicActivityPhaseUT_1();
+            AcademicActivityPhase academicActivityPhase = db.AcademicActivityPhases.Last();
+            CheckInRepo repo = new CheckInRepo();
+            CheckInRepo.infoParticipant obj = new CheckInRepo.infoParticipant
+            {
+                name = "duong",
+                participant_role_id = 101,
+                email = "duongpl@gmail.com",
+                participant_number = "12328181",
+                office_id = 2
+            };
+            bool res = repo.addParticipant(obj);
+            if (res)
+                Assert.Pass();
+        }
+    }
+}
